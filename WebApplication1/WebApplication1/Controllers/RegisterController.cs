1404fca3072048ff8ab817b684252a30509301af
Akshata Katre <akshataa2sk@gmail.com>
2020-08-26 20:38:40 +0530

    practice program

---
+++ WebApplication1/WebApplication1/Controllers/RegisterController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+namespace WebApplication1.Controllers
+{
+    public class RegisterController : Controller
+    {
+        // GET: Register
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(RegistrationClass rm, FormCollection fc)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.BasicPassword = fc["BasicPassword"];
+
+                ViewBag.StrongPassword = rm.ConfirmPassword;
+                return View(rm);
+            }
+            else
+            {
+                return View();
+            }
+
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Models/RegistrationClass.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public class RegistrationClass
+    {
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Address")]
+        [StringLength(200)]
+        public string Address { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Mobile No")]
+        [StringLength(10, ErrorMessage = "The Mobile must contains 10 characters", MinimumLength = 10)]
+        public string MobileNumber { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Email Address")]
+        [RegularExpression(".+@.+\\..+", ErrorMessage = "Please Enter Correct Email Address")]
+        public string Emailid { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Password")]
+        [StringLength(50, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Confirm Password")]
+        [StringLength(50, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+
+        public string ConformPassword { get; internal set; }
+        public string Age { get; set; }
+        public string Gender { get; set; }
+        public DateTime DateofBirth { get; set; }
+        public string Course { get; set; }
+        public DateTime RegistrationDate { get; set; }
+        public string Photo { get; set; }
+        public dynamic ConfirmPassword { get; internal set; }
+    }
+}
