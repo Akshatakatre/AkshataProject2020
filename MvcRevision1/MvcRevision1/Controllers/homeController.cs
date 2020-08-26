8cdca9dfbf4f24e2ad7ed23819b495f6071a2007
Akshata Katre <akshataa2sk@gmail.com>
2020-08-26 15:02:44 +0530

    practice program

---
+++ MvcRevision1/MvcRevision1/Controllers/homeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcRevision1.Controllers
+{
+    public class homeController : Controller
+    {
+        // GET: home
+        public ActionResult Index()
+        {
+
+            ViewBag.cn = "DIGITAL BUS PASS";
+            ViewData["address"] = "Nagpur";
+
+            return View();
+        }
+    }
+}
---
+++ MvcRevision1/MvcRevision1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MvcRevision1.MvcApplication" Language="C#" %>
