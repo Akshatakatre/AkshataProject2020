df194a6e635ccb7420547070c5322ca27d272181
Akshata Katre <akshataa2sk@gmail.com>
2020-08-26 20:41:24 +0530

    practice program

---
+++ WpfApp1/WpfApp1/Window1.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            int n1, n2, res;
+            n1 = Convert.ToInt32(textBox.Text);
+            n2 = Convert.ToInt32(textBox1.Text);
+            res = n1 + n2;
+            textBox2.Text = res.ToString();
+        }
+    }
+}
