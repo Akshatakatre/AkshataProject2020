9b16f7e4fdd92698763c71e6db021a0533d0a002
Akshata Katre <akshataa2sk@gmail.com>
2020-08-26 14:50:16 +0530

    practice program

---
+++ EFCodeFirstRegistrationForm/EFCodeFirstRegistrationForm/Models/StudentRegister.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace EFCodeFirstRegistrationForm.Models
+{
+    public class StudentRegister
+    {
+        public int Registrationid { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string MobileNumber { get; set; }
+        public string Emailid { get; set; }
+        public string Password { get; set; }
+        public string ConformPassword { get; set; }
+        public int Age { get; set; }
+        public string Gender { get; set; }
+        public DateTime DateofBirth { get; set; }
+        public string Course { get; set; }
+        public DateTime RegistrationDate { get; set; }
+    }
+}
