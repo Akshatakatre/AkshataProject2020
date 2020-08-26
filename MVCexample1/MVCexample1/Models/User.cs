90d5c4600457cdfe82d68b508b65dd9e1fbde846
Akshata Katre <akshataa2sk@gmail.com>
2020-08-26 15:01:20 +0530

    mvc exemple

---
+++ MVCexample1/MVCexample1/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCexample1.Models
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string MobileNumber { get; set; }
+        public string Emailid { get; set; }
+        public string Password { get; set; }
+        public string ConnformPassword { get; set; }
+        public string Age { get; set; }
+        public string Gender { get; set; }
+        public string DateofBirth { get; set; }
+        public string Course { get; set; }
+        public string RegistrationDate { get; set; }
+       
+    }
+}
