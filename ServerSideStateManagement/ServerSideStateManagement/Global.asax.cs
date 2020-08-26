8661d8016d21557bacb74ea6fcf411d62c00c784
Akshata Katre <akshataa2sk@gmail.com>
2020-08-26 20:28:31 +0530

    practice program

---
+++ ServerSideStateManagement/ServerSideStateManagement/Global.asax.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace ServerSideStateManagement
+{
+    public class Global : System.Web.HttpApplication
+    {
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            //code that run in application startup
+            // declaring one application variable and initialize it
+            Application["companyname"] = "MKPlacement";
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+            Session["username"] = "Rahul";
+            //code that runs when the new session will start
+            Response.Write("<br> session is now begining");
+            Response.Write("<br> session variable value :" + Session["username"].ToString());
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+            Response.Write("Request is now beging");
+            Response.Write("<br> Company Name:" + Application["companyname"].ToString());
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+            //code that run in application shut down
+        }
+    }
+}
