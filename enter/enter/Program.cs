a6c1ef00fd0f15caab2d4ad93c526f67369cbf9f
Akshata Katre <akshataa2sk@gmail.com>
2020-08-27 23:33:19 +0530

    practice program

---
+++ enter/enter/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace enter
+{
+    class Programenter
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("\n press Enter  to exit the program ");
+            while (Console.ReadKey().Key != ConsoleKey.Enter)
+            {
+                Console.WriteLine("press enter");
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
