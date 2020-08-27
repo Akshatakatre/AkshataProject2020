342c21db76eab64241db70bebd6673ed09067d9d
Akshata Katre <akshataa2sk@gmail.com>
2020-08-27 16:48:59 +0530

    practice programs

---
+++ akpnumber/akpnumber/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace akpnumber
+{
+    class numpri
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            i = 1;
+            while (i <= 10)
+            {
+                Console.WriteLine("number is:" + i);
+                i++;
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
