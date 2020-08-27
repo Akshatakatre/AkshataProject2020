cfc9fd8d6272cbbea55c803abc0fe6de347d6768
Akshata Katre <akshataa2sk@gmail.com>
2020-08-27 23:41:02 +0530

    practice rogram

---
+++ enumprogram/enumprogram/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace enumprogram
+{
+    class Program
+    {
+        enum grade { dist=75,first=60,second=50};
+        static void Main(string[] args)
+        {
+            
+        grade g = grade.dist;
+        int g1 = Convert.ToInt32(g);
+        Console.WriteLine("grade :"+g1);
+            if(g1>=75)
+            {
+            Console.WriteLine("very good");
+            }
+    Console.ReadKey();
+        }
+    }
+}
