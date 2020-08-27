30c3d3a19e510c6a9e5497dfa7637bd561507109
Akshata Katre <akshataa2sk@gmail.com>
2020-08-27 16:39:06 +0530

    practice program

---
+++ akoutparameter/akoutparameter/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace akoutparameter
+{
+    class Program
+    {
+        static void outm(out int n)
+        {
+            
+            
+        //    Console.WriteLine("value of i is:" + i);
+            
+        }
+        int i;
+        int n;
+        
+        static void Main(string[] args)
+        {
+            int n;
+            Console.WriteLine("enter the number");
+            n = Convert.ToInt32(Console.ReadLine());
+            outm(out n);
+            
+            Console.WriteLine("value of n:" + n);
+        }
+    }
+}
