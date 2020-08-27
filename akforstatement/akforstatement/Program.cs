f276a095b2b194b0ab244cc71b1225705522332d
Akshata Katre <akshataa2sk@gmail.com>
2020-08-27 12:20:59 +0530

    practice program

---
+++ akforstatement/akforstatement/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace akforstatement
+{
+    class statefor
+    {
+        static void Main(string[] args)
+        {
+            char name;
+            int i;
+            Console.WriteLine("enter the name");
+
+            name = Convert.ToChar(Console.ReadLine());
+            for(i=0;i<=2;i++)
+            {
+                Console.WriteLine(+name);
+            }
+            for(i=0;i<=2;i++)
+            {
+                Console.WriteLine(name);
+            }
+            Console.ReadKey();
+
+
+        }
+    }
+}
