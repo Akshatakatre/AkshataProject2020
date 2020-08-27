956335ed94b6b8aa8a4b152620a1e20a7c452b20
Akshata Katre <akshataa2sk@gmail.com>
2020-08-27 23:28:03 +0530

    practice program

---
+++ convert/convert/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculator
+{ 
+    
+    class Program
+    {
+  
+ 
+
+        static void Main(string[] args)
+        {
+            int x, y, result;
+            string op;
+            Console.WriteLine("enter number");
+            x =Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter another number");
+            y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter operator");
+           string op =Convert. (Console.WriteLine("press operator"));
+
+        }
+    }
+}
