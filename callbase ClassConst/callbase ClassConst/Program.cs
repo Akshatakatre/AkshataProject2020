6779147539e0f6217d66a5e654ee0eb6555b253f
Akshata Katre <akshataa2sk@gmail.com>
2020-08-27 23:14:00 +0530

    practice program

---
+++ callbase ClassConst/callbase ClassConst/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace callbase_ClassConst
+{
+    class Student
+    {
+        public Student(string name)
+        {
+            name = "Akshu";
+            Console.WriteLine("the name of the student is:" + name);
+        }
+    }
+    class Sdata:Student
+    {
+        public Sdata(string name):base (name)
+        {
+            Console.WriteLine(" the derive class name=" + name);
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sdata s = new Sdata("rd");
+            Console.ReadKey();
+        }
+    }
+}
