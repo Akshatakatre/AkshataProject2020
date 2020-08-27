2697825749900e4e6752261d7ad0430228a8505f
Akshata Katre <akshataa2sk@gmail.com>
2020-08-27 17:15:53 +0530

    practice program

---
+++ aktestthree/aktestthree/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aktestthree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1, n2, n3;
+            Console.WriteLine("enter the three numbers");
+            n1 = Convert.ToInt32(Console.ReadLine());
+            n2 = Convert.ToInt32(Console.ReadLine());
+            n3 = Convert.ToInt32(Console.ReadLine());
+            if(n1>n2 || n1>n3)
+            {
+                Console.WriteLine("n1 is greater");
+           }
+            else if(n2>n3)
+            {
+                Console.WriteLine("n2 is greater");
+            }
+            else
+            {
+                Console.WriteLine("n3 is greater");
+            }
+            Console.ReadKey();
+        }
+    }
+}
