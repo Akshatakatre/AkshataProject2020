a659a7c76d0126598bf89c1b3e9b491ac4d3cd7d
Akshata Katre <akshataa2sk@gmail.com>
2020-08-27 17:18:59 +0530

    practice program

---
+++ aktrycatch/aktrycatch/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aktrycatch
+{
+    class tcpro
+    {
+        static void Main(string[] args)
+        {
+            try
+            { 
+                int num1,num2, res;
+                Console.WriteLine("enter two numbers");
+                num1 = Convert.ToInt32(Console.ReadLine());
+                num2 = Convert.ToInt32(Console.ReadLine());
+            
+            
+                res = num1 / num2;
+                Console.WriteLine("result is:" + res);
+            }
+            catch(OverflowException e)
+            {
+                Console.WriteLine("number is long");
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            Console.WriteLine("execute all code");
+            Console.ReadKey();
+        }
+    }
+}
