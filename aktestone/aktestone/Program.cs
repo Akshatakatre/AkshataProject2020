65485c2d76dd4d80099dc7eb3a0b4b7252a689be
Akshata Katre <akshataa2sk@gmail.com>
2020-08-27 17:13:47 +0530

    practice program

---
+++ aktestone/aktestone/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aktestone
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float a, b, n1,n2,res;
+            Console.WriteLine("enter the two items weight");
+            a = Convert.ToSingle(Console.ReadLine());
+            b = Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine("enter the number of purchase");
+            n1 = Convert.ToSingle(Console.ReadLine());
+            n2 = Convert.ToSingle(Console.ReadLine());
+            // res = Convert.ToSingle(Console.ReadLine());
+            res = a + b / n1*n2;
+            Console.WriteLine("result is:" + res);
+            Console.ReadKey();
+        }
+    }
+}
