d0adf4b580738a1c032d4fcdf757bed18f59ccc5
Akshata Katre <akshataa2sk@gmail.com>
2020-08-27 17:08:01 +0530

    practice program

---
+++ akswitchexample/akswitchexample/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace akswitchexample
+{
+    class sscol
+    {
+        static void Main(string[] args)
+        {
+            char color;
+            Console.WriteLine("enter any character:");
+            color = Convert.ToChar(Console.ReadLine());
+            switch (color)
+            {
+               case 'R':
+                    Console.WriteLine("Red");
+                    break;
+                case 'B':
+                    Console.WriteLine("Blue");
+                    Console.WriteLine("Black");
+                    break;
+                    
+                case 'G':
+                    Console.WriteLine("Green");
+                    break;
+                default:
+                    Console.WriteLine("color not available");
+                    break;
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
