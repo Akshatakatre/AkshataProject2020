db49605813f4a0dae5a337ad9f334aa6e930ecc2
Akshata Katre <akshataa2sk@gmail.com>
2020-08-27 16:40:17 +0530

    practice program

---
+++ akpalendrom/akpalendrom/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace akpalendrom
+{
+    class Program
+    {
+        static int pale(ref int n)
+        {
+            int num, n1, n2, n3, rev;
+            
+            num = n;
+            n1 = num % 10;
+            num = num / 10;
+            n2 = num % 10;
+            num = num / 10;
+            n3 = num % 10;
+            num = num / 10;
+            rev = n1 * 100 + n2 * 10 + n3 * 1;
+            Console.WriteLine("reverse:" + rev);
+            if (rev==num)
+                Console.WriteLine("no is palendrom");
+            else
+                Console.WriteLine("no is not palendrom");
+            return rev;
+        }
+        static void Main(string[] args)
+        {
+            int n,x;
+
+            Console.WriteLine("enter 3 digit number");
+            n = Convert.ToInt32(Console.ReadLine());
+            x=pale(ref n);
+            Console.ReadKey();
+        }
+    }
+}
