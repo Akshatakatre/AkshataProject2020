3c7e6fa21a22a7eccc9330a32a3e4cf1de78c340
Akshata Katre <akshataa2sk@gmail.com>
2020-08-27 23:32:01 +0530

    practice program

---
+++ employee/employee/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace employee
+{
+    class EmpData
+    {
+        private int eid=02;
+        private string ename="akshu";
+        private float esal=23999;
+        public int empid
+        {
+            get
+            {
+                return eid;
+            }
+            set
+            {
+                eid = value;
+            }
+        }
+        public float empsal
+        {
+            get
+            {
+                return esal;
+            }
+            set
+            {
+                esal = value;
+            }
+        }
+        public string empname
+        {
+            get
+            {
+                return ename;
+            }
+
+        }
+    } 
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            EmpData e = new EmpData();
+            int a=e.empid;
+            string s = e.empname;
+            float f = e.empsal;
+            Console.WriteLine("employee id is:" + a);
+            Console.WriteLine("employee name is:" + s);
+            Console.WriteLine("employee salary is:" + f);
+            Console.ReadKey();
+
+        }
+    }
+}
