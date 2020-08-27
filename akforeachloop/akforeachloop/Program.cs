222aa46953caf497e35a7efbb915a012d0970420
Akshata Katre <akshataa2sk@gmail.com>
2020-08-27 12:17:32 +0530

    practice program

---
+++ akforeachloop/akforeachloop/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace akforeachloop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] num = new  int[3];
+            num[0] = 2;
+            num[1] = 6;
+            num[2] = 4;
+            foreach(int i in num)
+            {
+                Console.WriteLine(i);
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
