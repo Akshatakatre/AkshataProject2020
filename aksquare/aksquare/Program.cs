5a7b4276981e4e9f37865adf5ed601b2c48f1601
Akshata Katre <akshataa2sk@gmail.com>
2020-08-27 17:06:37 +0530

    practice program

---
+++ aksquare/aksquare/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aksquare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, squ;
+            Console.WriteLine("enter the number");
+            n = Convert.ToInt32(Console.ReadLine());
+            squ = n * n;
+            Console.WriteLine("square is:" + squ);
+            Console.ReadKey();
+
+        }
+    }
+}
