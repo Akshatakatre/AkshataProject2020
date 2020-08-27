c7296c1df41f34a2c434c39584e16ceed53dcd43
Akshata Katre <akshataa2sk@gmail.com>
2020-08-27 16:31:42 +0530

    practice program

---
+++ akfour/akfour/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace akfour
+{
+   public class Program
+    {
+        static void count(string s)
+        {
+            int sp=1;
+            int i;
+           // i = Convert.ToInt32(Console.ReadLine());
+           // Console.WriteLine("This is a test string");
+            for(i=0;i<s.Length;i++)
+            {
+                if(s[i]==' ')
+                {
+                   // Console.WriteLine("space=" + i);
+                    sp++;
+                }
+            }
+            Console.WriteLine("space is:" + sp);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter any sentence");
+            string str = Console.ReadLine();
+            count(str);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ akfour/four/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using akfour;
+namespace four
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Program p = new Program();
+            string s = "hello this is c# class";
+            int i = 4;
+            string str = s;
+            Assert.AreEqual(str, i);
+        }
+    }
+}
