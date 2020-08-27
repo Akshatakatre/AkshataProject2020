b89d670ff33d09e5b74d677fcb66a5cfbe0a7f22
Akshata Katre <akshataa2sk@gmail.com>
2020-08-27 16:59:16 +0530

    practice program

---
+++ akseven/akseven/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace akseven
+{
+   public class Program
+    {
+        static int cal(int x,int y)
+        {
+            int a, b,i;
+           // i = Convert.ToInt32(Console.ReadLine());
+            a = x;
+            b = y;
+            for(i=1;i<b;i++)
+            {
+                a = a * b;
+               // Console.WriteLine("result is:" + a);
+           }
+            Console.WriteLine("result is:" + a);
+            return a;
+        }
+        static void Main(string[] args)
+        {
+            int x, y;
+            Console.WriteLine("enter two number");
+            x = Convert.ToInt32(Console.ReadLine());
+            y = Convert.ToInt32(Console.ReadLine());
+            cal(x, y);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ akseven/sevena/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using akseven;
+namespace sevena
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Program p = new Program();
+            int a = 3;
+            int b = 2;
+            int c = 9;
+            int s = c;
+            Assert.AreEqual(s, 9);
+        }
+    }
+}
