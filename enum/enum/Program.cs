3e7af10c1f204957b5c67c9513f320b6df682de3
Akshata Katre <akshataa2sk@gmail.com>
2020-08-27 23:34:41 +0530

    practice program

---
+++ enum/enum/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace enum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            enum grade { dist=75,first=60,second=50};
+        grade g = grade.dist;
+        int g1 = Convert.ToInt32(g);
+        Console.WriteLine("grade: "+g1);
+            if(g>=75)
+            {
+            Console.WriteLine("very good");
+            }
+    Console.ReadKey();
+        }
+    }
+}
