12476e6e6b2fc4a041c5130fd41f0057465c3797
Akshata Katre <akshataa2sk@gmail.com>
2020-08-27 16:53:45 +0530

    practice program

---
+++ akptr/akptr/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace akptr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j;
+            for(i=1;i>=4;i++)
+            {
+                for(j= 4;j<0;j--)
+                {
+                    Console.Write("*");
+
+                }
+                Console.WriteLine("\n");
+            }
+            Console.ReadKey();
+        }
+        
+    }
+}
