1ff3612a725cfca253e124950002abe71b040f5b
Akshata Katre <akshataa2sk@gmail.com>
2020-08-27 17:05:20 +0530

    practice program

---
+++ aksix/aksix/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aksix
+{
+    
+    class Program
+    {
+        static void swap(int x, int y)
+        {
+            int a, b, temp;
+            a = x;
+            b = y;
+
+
+
+            temp = a;
+            a = b;
+            b = temp;
+            Console.WriteLine("first number is:" + a);
+            Console.WriteLine("second number is:" + b);
+
+
+        }
+        static void Main(string[] args)
+        {
+            int x, y;
+            Console.WriteLine("enter two numbers");
+            x = Convert.ToInt32(Console.ReadLine());
+            y = Convert.ToInt32(Console.ReadLine());
+            swap(x, y);
+            Console.ReadKey();
+
+        }
+    }
+}
