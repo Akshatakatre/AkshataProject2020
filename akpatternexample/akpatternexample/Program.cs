8fa446d65562c15a55d26742d43c09091f939f2f
Akshata Katre <akshataa2sk@gmail.com>
2020-08-27 16:43:01 +0530

    practice program

---
+++ akpatternexample/akpatternexample/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace akpatternexample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j;
+            for (i = 0; i <= 3; i++)
+            {
+                for(j=3;j>0;j--)
+                {
+                    Console.Write("*",+j);
+
+                }
+                Console.WriteLine("\n");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
