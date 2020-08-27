818303bc75d497f991006392306006897a0934a0
Akshata Katre <akshataa2sk@gmail.com>
2020-08-27 17:10:38 +0530

    practice program

---
+++ akprime/akprime/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace akprime
+{
+    class Program
+    {
+        static void prime()
+        {
+            int num,i=2;
+            Console.WriteLine("enter the number");
+            num = Convert.ToInt32(Console.ReadLine());
+            do
+            {
+                if (num % i == 0)
+                    Console.WriteLine("not prime number ");
+               // break;
+                i++;
+
+            } while (i <= num - 1);
+            if (num == i)
+                Console.WriteLine("no is prime");
+        }
+        static void Main(string[] args)
+        {
+            prime();
+            Console.ReadKey();
+        }
+    }
+}
