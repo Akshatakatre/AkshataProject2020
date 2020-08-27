4cb0c7aecf845701692909213c01dc9b270a6be3
Akshata Katre <akshataa2sk@gmail.com>
2020-08-27 23:30:32 +0530

    practice program

---
+++ Destructor/Destructor/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Destructor
+{
+    class car
+    {
+        public car()
+        {
+            Console.WriteLine("constructor is created");
+        }
+        ~car()
+        {
+            Console.WriteLine("destructor is called");
+            Console.ReadKey();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            car c = new car();
+            Console.ReadKey();
+        }
+    }
+}
