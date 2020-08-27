b893909b1845ae6d396569afc26d6a7c26efdb25
Akshata Katre <akshataa2sk@gmail.com>
2020-08-27 23:25:04 +0530

    practice program

---
+++ constructor/constructor/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace constructor
+{
+    class Number
+    {
+        public int num;
+        public Number()
+        {
+            num = 26;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number n = new Number();
+            Console.WriteLine("number is :" + n.num);
+            Console.ReadKey();
+        }
+    }
+}
