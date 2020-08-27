eec94aca191f5d8cdd57cfcbc2c733d26151d7ab
Akshata Katre <akshataa2sk@gmail.com>
2020-08-27 23:12:52 +0530

    practice program

---
+++ byte/byte/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @byte
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, sqr;
+            Console.WriteLine("enter the number");
+            n = Convert.ToInt16(Console.ReadLine());
+            sqr = Convert.ToInt16(n * n);
+            Console.WriteLine("square:" + sqr);
+            Console.ReadKey();
+
+        }
+    }
+}
