dfa7b521804641c3dde9d5e1298ab1769684ab89
Akshata Katre <akshataa2sk@gmail.com>
2020-08-27 12:09:30 +0530

    practice problem

---
+++ akfinally/akfinally/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace akfinally
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+
+            {
+                int num, n1, n2, n3, rev;
+                Console.WriteLine("enter the three digit number");
+                num = Convert.ToInt32(Console.ReadLine());
+                n1 = num % 10;
+                num = num / 10;
+                n2 = num % 10;
+                num = num / 10;
+                n3 = num % 10;
+                num = num / 10;
+                rev = n1 * 100 + n2 * 10 + n3 * 1;
+                Console.WriteLine("reverse:" + rev); 
+             }
+        catch(OverflowException e)
+            {
+                Console.WriteLine(e);
+            }
+            finally
+            {
+                Console.WriteLine("overall code is executed");
+            }
+            Console.ReadKey();
+        }
+    }
+}
