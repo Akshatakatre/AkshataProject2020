a4d2f5d470e518618f85be2083c7e280d93de50c
Akshata Katre <akshataa2sk@gmail.com>
2020-08-27 17:19:51 +0530

    practice program

---
+++ aktwo/aktwo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aktwo
+{
+    class Program
+    {
+        static string dis()
+        {
+            string str1;
+            //Console.WriteLine("enter the name");
+           // string name = Convert.ToString(Console.ReadLine());
+            str1 = "Welcome Friend Jhon \n Have a nice day";
+                
+            
+            return str1 ;
+        }
+        static void Main(string[] args)
+        {
+           string str= dis();
+            Console.WriteLine(str);
+            Console.ReadKey();
+        }
+    }
+}
