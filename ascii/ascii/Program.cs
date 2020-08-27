efe0350a565b3f448391e472e93d2310bcb0c190
Akshata Katre <akshataa2sk@gmail.com>
2020-08-27 23:08:06 +0530

    practice program

---
+++ ascii/ascii/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ascii
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the character");
+            int c = Console.Read();
+            Console.WriteLine("ascii_value=" + c);
+            Console.WriteLine("c=" + Convert.ToChar(c));
+            Console.ReadKey();
+
+        }
+    }
+}
