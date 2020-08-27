b82ad89cfc7210893df8d96b318756c75d31ffeb
Akshata Katre <akshataa2sk@gmail.com>
2020-08-27 23:39:17 +0530

    practice program

---
+++ EventExample/EventExample/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventExample
+{
+    class Account
+    {
+        public int balance = 1000;
+        public string deposit(int amt)
+        {
+            balance = balance + amt;
+            return "amount depositated balance is:" + balance;
+        }
+        public string withdraw(int amt)
+        {
+            balance = balance - amt;
+            return "amount withdraw is " + balance;
+        }
+    }
+    class Program
+    {
+        public delegate string accountDel(int amt);
+        private event accountDel accevent;
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            Console.WriteLine("enter the amount");
+            int amount = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter deposit or withdraw");
+            string t = Console.ReadLine();
+
+            Account a = new Account();
+            accountDel ad = null;
+            switch(t)
+            {
+                case "deposit":
+                    p.accevent += new accountDel(a.deposit);
+                    break;
+                case "withdraw":
+                    p.accevent += new accountDel(a.withdraw);
+                    break;
+                default:
+                    Console.WriteLine("please enter deposit or withdraw");
+                    break;
+            }   
+            if(p.accevent!=null)
+            {
+                string result = p.accevent(amount);
+                Console.WriteLine(result);
+            }
+            Console.ReadKey();
+        }
+    }
+}
