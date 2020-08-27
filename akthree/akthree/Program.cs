f119f6102b23606f0635767dd0b1c7a92653e1f7
Akshata Katre <akshataa2sk@gmail.com>
2020-08-27 17:18:09 +0530

    practice program

---
+++ akthree/akthree/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace akthree
+{
+    class Program
+    {
+        static void sum(int x,int y)
+        {
+            int a, b, c;
+            a = x;
+            b = y;
+            //c = Convert.ToInt32(Console.ReadLine());
+            c = a + b;
+            Console.WriteLine("result is:" + c);
+        }
+        static void Main(string[] args)
+        {
+            int x, y;
+            Console.WriteLine("enter two numbers");
+            x = Convert.ToInt32(Console.ReadLine());
+            y = Convert.ToInt32(Console.ReadLine());
+            sum(x, y);
+            Console.ReadKey();
+        }
+    }
+}
