f7d86bb2dc1a817e67c5fb852d27fa3b3d63de69
Akshata Katre <akshataa2sk@gmail.com>
2020-08-27 23:20:14 +0530

    practice program

---
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentDetails
+{
+    class Student
+    {
+        public string Name { get; set; }
+        public int Roll_no { get; set; }
+        public void getData()
+        {
+            Name = "John";
+            Roll_no = 2;
+        }
+        public void showData()
+        {
+            Console.WriteLine("student name is:" + Name);
+            Console.WriteLine("student roll no is:" + Roll_no);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s = new Student();
+            s.getData();
+            s.showData();
+            Console.ReadKey();
+        }
+    }
+}
