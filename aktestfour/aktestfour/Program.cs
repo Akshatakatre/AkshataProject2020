945b3659a7138738e8c1dd08f0f30a278ab1e21a
Akshata Katre <akshataa2sk@gmail.com>
2020-08-27 17:12:41 +0530

    practice program

---
+++ aktestfour/aktestfour/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aktestfour
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int td;
+            float f,average;
+            Console.WriteLine("enter the total distance");
+            td = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter the amount of fuel ");
+            f = Convert.ToSingle(Console.ReadLine());
+            average = td / f;
+            Console.WriteLine("average of the bike is:" + average);
+            Console.ReadKey();
+
+        }
+    }
+}
