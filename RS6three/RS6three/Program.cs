0dd1ac87b6ecaf78919d8b0d70b42be28c6ebd6b
Akshata Katre <akshataa2sk@gmail.com>
2020-08-28 12:51:55 +0530

    practice program

---
+++ RS6three/RS6three/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RS6three
+{
+   public class Reverse
+    {
+        public int number { get; set; }
+        public int number1 { get; set; }
+        public int number2 { get; set; }
+        public int number3 { get; set; }
+        public int rev { get; set; }
+        public void numRev()
+        {
+            Console.WriteLine("enter three digit number");
+            number = int.Parse(Console.ReadLine());
+            Console.WriteLine("the number is:" + number);
+            number1 = number % 10;
+            number = number / 10;
+            number2 = number % 10;
+            number = number / 10;
+            number3 = number % 10;
+            number = number / 10;
+            int res = number1 * 100 + number2 * 10 + number3 * 1;
+            Console.WriteLine("the reverse of the number is:" + res);
+        }
+        
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Reverse r = new Reverse();
+                r.numRev();
+            Console.ReadKey();
+        }
+    }
+}
