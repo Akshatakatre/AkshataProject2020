cfa941ef3f4aafea0941af6ec4b79f491d062928
Akshata Katre <akshataa2sk@gmail.com>
2020-08-28 12:27:59 +0530

    practice program

---
+++ parameterconstructor/parameterconstructor/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace parameterconstructor
+{
+    public class Person
+    {
+        private string first;
+        private string last;
+        public Person(string fname,string lname)
+        {
+            first = fname;
+            last = lname;
+        }
+        public void show()
+        {
+            Console.WriteLine("first name is:" + first);
+            Console.WriteLine("last name is:" + last);
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person("akshu", "katre");
+            p.show();
+            Console.ReadKey();
+        }
+    }
+}
