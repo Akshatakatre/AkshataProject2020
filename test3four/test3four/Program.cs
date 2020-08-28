a050e35deb5ac2bfc158e46457a743975a48ca90
Akshata Katre <akshataa2sk@gmail.com>
2020-08-28 13:15:09 +0530

    practice program

---
+++ test3four/test3four/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test3four
+{
+    class Program
+    { 
+        public void divide()
+        {
+            int r;
+            Console.WriteLine("enter two numbers");
+           int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+            if(n1>n2)
+            {
+                r = n1 / n2;
+                Console.WriteLine("result is :" + r);
+                Console.WriteLine("division is possible");
+            }
+            else
+            {
+                Console.WriteLine("division not possible");
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.divide();
+            Console.ReadKey();
+        }
+    }
+}
