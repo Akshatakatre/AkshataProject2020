845f929a9dca6a59bc5d4e31f7269bfdc55f747a
Akshata Katre <akshataa2sk@gmail.com>
2020-08-28 13:21:00 +0530

    practice program

---
+++ test3seven/test3seven/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test3seven
+{
+    class Program
+    {
+        public void print()
+        {
+            int p;
+            string n1;
+            Console.WriteLine("enter the three number");
+           n1=Convert.ToString( Console.ReadLine());
+           // n2 = int.Parse(Console.ReadLine());
+           // n3 = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter the number for how many lines are print");
+            p = int.Parse(Console.ReadLine());
+            for(int i=0;i< p;i++)
+            {
+                Console.WriteLine(n1);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.print();
+            Console.ReadKey();
+        }
+    }
+}
