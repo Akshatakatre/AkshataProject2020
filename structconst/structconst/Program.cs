37627553f444721cd7d06ea045b1a9144c549539
Akshata Katre <akshataa2sk@gmail.com>
2020-08-28 13:01:04 +0530

    practice program

---
+++ structconst/structconst/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace structconst
+{
+    public struct constr
+    {
+        public string fname;
+        public int age;
+
+
+        public constr (string a,int b)
+            {
+            fname = a;
+            age = b;
+
+            }
+        public void sdata()
+        {
+            Console.WriteLine("first name is:" + fname);
+            Console.WriteLine("age is :" + age);
+        }
+    }
+    class sprogram
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the name and age");
+            string a = Console.ReadLine();
+            int b  = Convert.ToInt32(Console.ReadLine());
+            constr s = new constr(a, b);
+            s.sdata();
+            Console.ReadKey();
+        }
+    }
+}
