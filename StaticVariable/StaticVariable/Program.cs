33ea5af1f82b4d12a5909d7665fc63431d563eeb
Akshata Katre <akshataa2sk@gmail.com>
2020-08-28 12:58:36 +0530

    practice program

---
+++ StaticVariable/StaticVariable/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticVariable
+{
+    class Uname
+    {
+        private static string name;
+        public void set(string name)
+        {
+            Uname.name = name;
+        }
+        public static void show()
+        {
+            Console.WriteLine("name is:" + name);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Uname u = new Uname();
+            u.set("akshu");
+            Uname.show();
+            Console.ReadKey();
+        }
+    }
+}
