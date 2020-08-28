3c76841cda1353c1b327d73ae295ced129a48505
Akshata Katre <akshataa2sk@gmail.com>
2020-08-28 12:59:51 +0530

    practice program

---
+++ short/short/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @short
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            short n, sqr;
+            Console.WriteLine("enter the number");
+            n = Convert.ToInt16(Console.ReadLine());
+            sqr = Convert.ToInt16(n * n);
+            Console.WriteLine("square:" + sqr);
+            Console.ReadKey();
+        }
+    }
+}
