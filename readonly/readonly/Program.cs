fed08770e335c03e9dd5dd23a5a65eaf9f5056e9
Akshata Katre <akshataa2sk@gmail.com>
2020-08-28 12:33:02 +0530

    practice program

---
+++ readonly/readonly/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @readonly
+{
+    class set
+    {
+        public readonly string name = "akshu";
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            set s = new set();
+            string st = s.name;
+            Console.WriteLine("the name is :" + st);
+            Console.ReadLine();
+
+        }
+    }
+}
