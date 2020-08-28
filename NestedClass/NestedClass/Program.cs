3c729c38e639aeeb8657536403151f106ef25797
Akshata Katre <akshataa2sk@gmail.com>
2020-08-28 12:26:48 +0530

    practice program

---
+++ NestedClass/NestedClass/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NestedClass
+{
+    class Uname
+    {
+        public class nam
+        {
+            private string name;
+            public void set(string n)
+            {
+                name = n;
+                Console.WriteLine("name is:" + name);
+
+            }
+        }
+        public int actno;
+        public void setaccno(int a)
+        {
+            actno = a;
+            Console.WriteLine("account no is:" + actno);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Uname.nam n = new Uname.nam();
+            n.set("akshu");
+            Uname u = new Uname();
+            u.setaccno(02);
+            Console.ReadKey();
+        }
+    }
+}
