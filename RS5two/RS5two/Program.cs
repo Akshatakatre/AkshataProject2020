6e7af0d84d59b38afbcf40912560bbd348e0a41b
Akshata Katre <akshataa2sk@gmail.com>
2020-08-28 12:46:15 +0530

    practice program

---
+++ RS5two/RS5two/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RS5two
+{
+    class Geometric
+    {
+        public int number { get; set; }
+      
+        public void series(int num)
+        {
+            number = num;
+            int i = 5;
+            for ( i=5;i< number;)
+            {
+                Console.WriteLine("the geometric series is:" + i);
+               
+                i = i +i;
+               
+            }
+           
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Geometric g = new Geometric();
+            Console.WriteLine("enter the number");
+            int num = int.Parse(Console.ReadLine());
+            g.series(num);
+            Console.ReadKey();
+        }
+    }
+}
