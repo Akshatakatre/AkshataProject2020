f33d1ac9867bd4afb11c64adfa04e65242a74451
Akshata Katre <akshataa2sk@gmail.com>
2020-08-28 12:15:53 +0530

    practice program

---
+++ Inheritance2/Inheritance2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance2
+{
+    class Parent
+    {
+        private void show()
+        {
+            Console.WriteLine("this is parent class");
+        }
+    }
+    class Child:Parent
+    {
+        public void display()
+        {
+            Console.WriteLine("this is child class");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Parent p = new Parent();
+            Child c = new Child();
+            p.show();
+            c.display();
+            c.show();
+            Console.ReadKey();
+        }
+    }
+}
