2ffd8ac1d165c42c599f5d6cb246d9f03788d135
Akshata Katre <akshataa2sk@gmail.com>
2020-08-28 12:44:04 +0530

    practice program

---
+++ RS5one/RS5one/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RS5one
+{
+    class Arithmetic
+    {
+        public int number { get; set; }
+        public void series(int num)
+        {
+            number = num;
+            for(int i=1;i< number;)
+            {
+                i= i + 3;
+                Console.WriteLine("the arithmetic series is:" + i);
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Arithmetic a = new Arithmetic();
+            Console.WriteLine("enter the number");
+            int num = int.Parse(Console.ReadLine());
+            a.series(num);
+            Console.ReadKey();
+        }
+    }
+}
