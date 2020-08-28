1527ccf554cb7c53821f9d4c7c8071349e214b75
Akshata Katre <akshataa2sk@gmail.com>
2020-08-28 13:03:23 +0530

    practice program

---
+++ square/square/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace square
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, sqr;
+            Console.WriteLine("enter the number");
+           
+            n = Convert.ToInt32(Console.ReadLine());
+            sqr = n * n;
+            Console.WriteLine("square: " + sqr);
+            Console.ReadKey();
+        }
+    }
+}
