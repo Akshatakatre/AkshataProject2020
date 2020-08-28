881a0b5a77ac6f8e8f7b03393b05dd5849c44a4f
Akshata Katre <akshataa2sk@gmail.com>
2020-08-28 12:50:47 +0530

    ptactice program

---
+++ RS6one/RS6one/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RS6one
+{
+    class Check
+    {
+        public int number1 { get; set; }
+        public int number2 { get; set; }
+        public void Swap()
+        {
+
+            Console.WriteLine("enter the number1");
+            number1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter the number2");
+            number2 = int.Parse(Console.ReadLine());
+            int res = number1 + number2;
+            res = res - number1;
+            Console.WriteLine("first number swap is" + res);
+            res = number1 + number2;
+            res =  res- number2;
+            Console.WriteLine("second number swap is" + res);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Check c = new Check();
+            c.Swap();
+            Console.ReadKey();
+        }
+    }
+}
