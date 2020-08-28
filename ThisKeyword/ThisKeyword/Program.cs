753180197ad745405fcb4e6cd3e42a97c35b107a
Akshata Katre <akshataa2sk@gmail.com>
2020-08-28 13:35:25 +0530

    practice program

---
+++ ThisKeyword/ThisKeyword/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThisKeyword
+{
+    class Bankacc
+    {
+        private string name;
+        public void SetName(string name)
+        {
+            this.name = name;
+        }
+        public void Show()
+        {
+            Console.WriteLine("name is:" + name);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bankacc b = new Bankacc();
+            b.SetName("akshu");
+            b.Show();
+            Console.ReadKey();
+        }
+    }
+}
