19e004f4b93e99f2463ada8cfc9bda21667376cc
Akshata Katre <akshataa2sk@gmail.com>
2020-08-28 13:02:07 +0530

    practice program

---
+++ structureprogram/structureprogram/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace structureprogram
+{
+    public struct Employee
+    {
+        public string fname;
+        public int age;
+
+    }
+    class strupro
+    {
+        static void Main(string[] args)
+        {
+            Employee e;
+            Console.WriteLine("enter name and age");
+            e.fname = Console.ReadLine();
+            e.age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("first name is:" + e.fname);
+            Console.WriteLine("age is:" + e.age);
+            Console.ReadKey();
+
+
+        }
+    }
+}
