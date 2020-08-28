1a3b234ce56c4555da591ade77fb040a9b31d1c0
Akshata Katre <akshataa2sk@gmail.com>
2020-08-28 12:22:44 +0530

    practice program

---
+++ InterfaceE/InterfaceE/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceE
+{
+    interface Employee
+    {
+        string eName { get; set; }
+    }
+    interface EmpData
+    {
+        void set(string n);
+    }
+    class Edetails:Employee,EmpData
+    {
+        public string eName()
+        {
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
