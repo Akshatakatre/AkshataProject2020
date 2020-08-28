c86fb13b8f25ee3ba5a9a19aa42d24f5ff9df778
Akshata Katre <akshataa2sk@gmail.com>
2020-08-28 12:34:03 +0530

    practice program

---
+++ privateconstructor/privateconstructor/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace privateconstructor
+{
+    class privateconst
+    {
+        public static string name="akshata";
+        private privateconst()
+        {
+
+        }
+        public static void show()
+        {
+            name = "ak";
+            Console.WriteLine("the name is:" + name);
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            privateconst.name ="akshu";
+            privateconst.show();
+            Console.ReadKey();
+
+        }
+    }
+}
