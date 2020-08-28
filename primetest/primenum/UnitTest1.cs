e9ec4f77aa602bb8e6173dad7c49f42e4212dcc0
Akshata Katre <akshataa2sk@gmail.com>
2020-08-28 12:29:57 +0530

    practice program

---
+++ primetest/primenum/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using primetest;
+
+namespace primenum
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Test_check()
+        {
+            Program p = new Program();
+            string res =p.check(7);
+            string accept = "the number is prime ";
+            string excepted = res;
+            Assert.AreEqual(excepted, accept);
+
+        }
+    }
+}
---
+++ primetest/primetest/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace primetest
+{
+        
+    public class Program
+    {
+        public  string check(int a)
+        {
+            int n, i = 2;
+            string str=null;
+            //Console.WriteLine("enter the number");
+            n = a;
+           // n = Convert.ToInt32(Console.ReadLine());
+            // i = Convert.ToInt32(Console.ReadLine());
+            do
+            {
+                if (n % i == 0)
+                {
+                    str="number is not prime";
+                    break;
+                }
+                i++;
+
+            } while (i <= n - 1);
+            if (n == i)
+            {
+                str="the number is prime";
+            }
+            return str;
+        }
+
+
+
+        static void Main(string[] args)
+        {
+        Program p = new Program();
+           Console.WriteLine("enter the number");
+           // int a = Convert.ToInt32(Console.ReadLine());
+            int a=int.Parse(Console.ReadLine());
+            String s=p.check(a);
+            //Console.WriteLine("result is:"+)
+            Console.WriteLine("output is:" + s);
+        Console.ReadKey();
+        }
+    }
+}
