c9e9be721c76027895ae26c5d3b5cef602c5c178
Akshata Katre <akshataa2sk@gmail.com>
2020-08-28 12:39:16 +0530

    practice program

---
+++ RS4One/RS4One/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RS4One
+{
+    public class Calculate
+    {
+        public float sum=1.0f;
+        public void cal()
+        {
+            for (float denominator = 2; denominator <= 50; denominator++)
+            {
+                sum = sum + (1 / denominator);
+            }
+            Console.WriteLine("sum is" + sum);
+        }
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Calculate c = new Calculate();
+                c.cal();
+                Console.ReadKey();
+
+            }
+        }
+    }
+}
