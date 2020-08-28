76b38750a9def05788c5cba16b0d7a34e865c3b0
Akshata Katre <akshataa2sk@gmail.com>
2020-08-28 13:26:22 +0530

    practice program

---
+++ test3two/test3two/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test3two
+{
+   public  class pass
+    {
+        public  int i;  
+        public  int p=1234;
+        public  void check()
+        {
+            
+            Console.WriteLine("enter the 4 digit password");
+            i = Convert.ToInt32(Console.ReadLine());
+            if (p==i)
+            {
+                Console.WriteLine("correct password");
+            }
+            else 
+            {
+                Console.WriteLine("incorrect password");
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+       
+          
+            pass p = new pass();
+           p. check();
+            Console.ReadKey();
+            
+        }
+    }
+}
