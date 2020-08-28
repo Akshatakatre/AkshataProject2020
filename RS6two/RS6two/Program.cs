2914726141b96c595f6cccf6f7e90bb465f268d6
Akshata Katre <akshataa2sk@gmail.com>
2020-08-28 12:54:10 +0530

    practice program

---
+++ RS6two/RS6two/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RS6two
+{
+    class Move
+    {
+        public int number { get; set; }
+        public void shift()
+        {
+            Console.WriteLine("enter the number");
+            number = int.Parse(Console.ReadLine());
+            number <<= 2;
+            Console.WriteLine("the result is:" + number);        
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Move m = new Move();
+            m.shift();
+            Console.ReadKey();
+        }
+    }
+}
