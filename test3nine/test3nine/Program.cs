50380542d82420105340f9308905bac3df4c9898
Akshata Katre <akshataa2sk@gmail.com>
2020-08-28 13:16:18 +0530

    practice program

---
+++ test3nine/test3nine/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test3nine
+{
+    class Program
+    {
+        public void print()
+        {
+            int p, q;
+            Console.WriteLine("enter two numbers");
+            p = int.Parse(Console.ReadLine());
+            q = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter the no of lines to print");
+            int r = int.Parse(Console.ReadLine());
+            for(int i=0;i< r;i++)
+            {
+                Console.WriteLine("value of p is:" + p);
+            }
+        } 
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.print();
+            Console.ReadKey();
+        }
+    }
+}
