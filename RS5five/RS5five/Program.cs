5d7b08e839af1f511b49556297ff16c75debb983
Akshata Katre <akshataa2sk@gmail.com>
2020-08-28 12:41:33 +0530

    practice program

---
+++ RS5five/RS5five/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RS5five
+{
+    class Tringular
+    {
+        public int number { get; set; }
+        public int sum { get; set; }
+        public void series(int num)
+        {
+            number = num;
+            for(int Number=1;Number< number;Number++)
+            {
+                Console.WriteLine("the tringular series is:" + sum);
+                sum = sum + Number;
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tringular t = new Tringular();
+            Console.WriteLine("enter the number");
+            int num = int.Parse(Console.ReadLine());
+            t.series(num);
+            Console.ReadKey();
+        }
+    }
+}
