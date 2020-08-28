5bc2ef8edc4e8cb1ed554b68de467c2aa4245948
Akshata Katre <akshataa2sk@gmail.com>
2020-08-28 12:40:23 +0530

    practice program

---
+++ RS4Two/RS4Two/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RS4Two
+{
+    public class Number
+    {
+        public float sum=1.0f;
+        public void cal()
+        {
+            for (int denominator = 2; denominator <= 8; )
+            {
+                for (int divident = 3; divident <= 7; )
+                {
+                    sum = sum + (divident / denominator);
+                    divident = divident + 2;
+                    denominator = denominator + 2;
+                }
+                //denominator = denominator + 2;
+            }
+            Console.WriteLine("sum is:" + sum);
+        }
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Number n = new Number();
+                n.cal();
+                Console.ReadKey();
+            }
+        }
+    }
+}
