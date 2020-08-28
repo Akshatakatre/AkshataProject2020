342897a679650ed9f418c4dc499e50f616acd8ca
Akshata Katre <akshataa2sk@gmail.com>
2020-08-28 12:25:50 +0530

    practice program

---
+++ long/long/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @long
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long n,sqr;
+            Console.WriteLine("enter the number");
+            n = Convert.ToInt64(Console.ReadLine());
+            sqr = Convert.ToInt64(n * n);
+            Console.WriteLine("square:" + sqr);
+            Console.ReadKey();
+
+        }
+    }
+}
