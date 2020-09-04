9b6620c1429ff4ddd2403d52583c5b4bf5dadf45
Akshata Katre <akshataa2sk@gmail.com>
2020-09-04 23:12:02 +0530

    practice program

---
+++ ClassExample/ClassExample/Users.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ClassExample
+{
+    public class Users
+    {
+        public int userid { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Emailid { get; set; }
+        public string Mobilenumber { get; set; }
+        public string imagename { get; set; }
+        public string Role { get; set; }
+
+        public Users(int ui,string un,string p,string ei,string mn,string imgn,string r)
+        {
+            userid = ui;
+            UserName = un;
+            Password = p;
+            Emailid = ei;
+            Mobilenumber = mn;
+            imagename = imgn;
+            Role = r;
+        }
+    }
+}
