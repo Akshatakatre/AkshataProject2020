41ef7fbf41f0cec132d93a4d19adca3443af8ca6
Akshata Katre <akshataa2sk@gmail.com>
2020-09-24 10:32:53 +0530

    practice program

---
+++ Delegate6/Delegate6/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Delegate6
+{
+    public delegate void DelEventHandler();
+
+    class Program
+    {
+        public static event DelEventHandler add;
+
+        static void Main(string[] args)
+        {
+            add += new DelEventHandler(USA);
+            add += new DelEventHandler(India);
+            add += new DelEventHandler(England);
+            add.Invoke();
+
+            Console.ReadKey();
+        }
+        static void USA()
+        {
+            Console.WriteLine("USA");
+        }
+
+        static void India()
+        {
+            Console.WriteLine("India");
+        }
+
+        static void England()
+        {
+            Console.WriteLine("England");
+        }
+    }
+}
+
