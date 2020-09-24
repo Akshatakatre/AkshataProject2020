6ce4dbd48ade71a4be0e35845a171adf4e8c4b21
Akshata Katre <akshataa2sk@gmail.com>
2020-09-24 10:43:18 +0530

    practice program

---
+++ Delegate8/Delegate8/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Delegate8
+{
+    public delegate void NameDelegate(string msg);
+
+    public class Person
+    {
+        public string firstName;
+        public string secondName;
+
+        public Person(string firstName, string secondName)
+        {
+            this.firstName = firstName;
+            this.secondName = secondName;
+        }
+
+        public void ShowFirstName(string msg)
+        {
+            Console.WriteLine(msg + this.firstName);
+        }
+
+        public void ShowSecondName(string msg)
+        {
+            Console.WriteLine(msg + this.secondName);
+        }
+    }
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var per = new Person("Akshata", "Katre");
+
+            var nDelegate = new NameDelegate(per.ShowFirstName);
+            nDelegate("First name : ");
+
+            nDelegate = new NameDelegate(per.ShowSecondName);
+            nDelegate("Last name : ");
+            Console.ReadKey();
+        }
+    }
+}
