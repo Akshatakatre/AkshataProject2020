cc314b84dbcb0b087783730e76e2ad679e8d0008
Akshata Katre <akshataa2sk@gmail.com>
2020-09-24 09:58:58 +0530

    practice program

---
+++ Delegate2/Delegate2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Delegate2
+{
+    class Rectangle
+    {
+        public delegate void rectangle(double height, double width);
+        
+        public void area(double height,double width)
+        {
+            Console.WriteLine("Area of rectangle is:{0}", width * height);
+        }
+
+        public void perimeters(double height,double width)
+        {
+            Console.WriteLine("perimeter is :{0}", 2 * (height + width));
+        }
+        static void Main(string[] args)
+        {
+            Rectangle rect = new Rectangle();
+
+            rectangle rectdele = new rectangle(rect.area);
+            rectdele += rect.perimeters;
+            rectdele.Invoke(6.3, 4.2);
+            Console.WriteLine();
+            rectdele.Invoke(16.3, 10.3);
+            Console.ReadLine();
+
+
+        }
+    }
+}
