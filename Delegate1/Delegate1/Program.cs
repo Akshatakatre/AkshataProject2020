35e561c4edccc6e968e88539e5ef9e9123c5dab7
Akshata Katre <akshataa2sk@gmail.com>
2020-09-24 09:53:09 +0530

    practice program

---
+++ Delegate1/Delegate1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Delegate1
+{
+    class Program
+    {
+        public delegate void addnum(int a, int b);
+        public delegate void subnum(int a, int b);
+
+        public void sum(int a,int b)
+        {
+            Console.WriteLine("(100+26)={0}", a + b);
+        }
+
+        public void substract(int a,int b)
+        {
+            Console.WriteLine("(100-20)={0}", a - b);
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            addnum del_obj1 = new addnum(p.sum);
+            subnum del_obj2 = new subnum(p.substract);
+
+            del_obj1(100, 26);
+            del_obj2(100, 20);
+            Console.ReadKey();
+
+        }
+    }
+}
