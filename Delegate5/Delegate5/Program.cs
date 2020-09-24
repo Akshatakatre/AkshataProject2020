53eddfa52725475c85b05e63c44e3cc9e2a79104
Akshata Katre <akshataa2sk@gmail.com>
2020-09-24 10:06:26 +0530

    practice program

---
+++ Delegate5/Delegate5/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Delegate5
+{
+    public class Operation
+    {
+        public static void One()
+        {
+            Console.WriteLine("Hello... one.... ");
+            throw new Exception("Error");
+        }
+        public static void Two()
+        {
+            Console.WriteLine("Hello...... Two.....");
+        }
+    }
+    class Program
+    {
+        delegate void DelOp();
+
+        static void Main(string[] args)
+        {
+            DelOp obj = Operation.One;
+            obj += Operation.Two;
+
+            Delegate[] del = obj.GetInvocationList();
+
+            foreach (DelOp d in del)
+            {
+                try
+                {
+                    d();
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Error caught");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
