52ba304a0410ae334aee92fa2c71d417335b8a42
Akshata Katre <akshataa2sk@gmail.com>
2020-09-24 11:04:36 +0530

    practice program

---
+++ FileHandlingExample/FileHandlingExample/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+
+namespace FileHandlingExample
+{
+    class Program
+    {
+        class WriteToFile
+        {
+
+            public void Data()
+            {
+                // This will create a file named sample.txt 
+                // at the specified location  
+                StreamWriter sw = new StreamWriter("E://practice.txt");
+
+                // To write on the console screen 
+                Console.WriteLine("Enter the Text that you want to write on File");
+
+                // To read the input from the user 
+                string str = Console.ReadLine();
+
+                // To write a line in buffer 
+                sw.WriteLine(str);
+
+                // To write in output stream 
+                sw.Flush();
+
+                // To close the stream 
+                sw.Close();
+            }
+        }
+        static void Main(string[] args)
+        {
+            WriteToFile wr = new WriteToFile();
+            wr.Data();
+            Console.ReadKey();
+        }
+    }
+}
