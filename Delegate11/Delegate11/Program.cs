5e7fe9a7b56bcbd1d4fb141c393f618ff2bd0cdf
Akshata Katre <akshataa2sk@gmail.com>
2020-09-24 10:10:43 +0530

    practice program

---
+++ Delegate11/Delegate11/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Delegate11
+{
+    public delegate void OnFiveHandler(object sender, EventArgs e);
+
+    class FEvent
+    {
+        public event OnFiveHandler FiveEvent;
+
+        public void OnFiveEvent()
+        {
+            if (FiveEvent != null)
+            {
+                FiveEvent(this, EventArgs.Empty);
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fe = new FEvent();
+            fe.FiveEvent += new OnFiveHandler(Callback);
+
+            var random = new Random();
+
+            for (int i = 0; i < 10; i++)
+            {
+                int rn = random.Next(6);
+
+                Console.WriteLine(rn);
+
+                if (rn == 5)
+                {
+                    fe.OnFiveEvent();
+                }
+            }
+            Console.ReadKey();
+        }
+
+        public static void Callback(object sender, EventArgs e)
+        {
+            Console.WriteLine("Five Event occurred");
+        }
+    }
+    
+}
