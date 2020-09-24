73117752d1e7866780954df0349469c1b6d777c2
Akshata Katre <akshataa2sk@gmail.com>
2020-09-24 10:49:12 +0530

    practice program

---
+++ Delegate9/Delegate9/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Delegate9
+{
+    delegate void MyDelegate();
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDelegate del = delegate
+            {
+                Console.WriteLine("Anonymous method");
+            };
+
+            del();
+            Console.ReadKey();
+        }
+    }
+}
