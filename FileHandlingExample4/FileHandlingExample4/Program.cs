8f0fede79238366e1fb7d57ed77ecd9ef68a4e04
Akshata Katre <akshataa2sk@gmail.com>
2020-09-25 15:26:20 +0530

    practice program

---
+++ FileHandlingExample4/FileHandlingExample4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileHandlingExample4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream fs = new FileStream("E:\\csharpfile.txt", FileMode.Append);
+            byte[] bdata = Encoding.Default.GetBytes("Hello File Handling!");
+            fs.Write(bdata, 0, bdata.Length);
+            fs.Close();
+            Console.WriteLine("Successfully saved file with data : Hello File Handling!");
+            Console.ReadKey();
+        }
+    }
+}
