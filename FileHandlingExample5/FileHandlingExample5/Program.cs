1e362fba555ca69ef00246795c72c7297cfe6363
Akshata Katre <akshataa2sk@gmail.com>
2020-09-25 15:27:27 +0530

    practice program

---
+++ FileHandlingExample5/FileHandlingExample5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileHandlingExample5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream fs = new FileStream("E:\\cshsrpfile.txt", FileMode.Create);
+            fs.Close();
+            Console.Write("File has been created and the Path is D:\\csharpfile.txt");
+            Console.ReadKey();
+        }
+    }
+}
