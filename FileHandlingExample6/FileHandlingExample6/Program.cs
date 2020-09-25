342aab17dbfb16146c17dfc870935816f3ae93fa
Akshata Katre <akshataa2sk@gmail.com>
2020-09-25 15:28:36 +0530

    practice program

---
+++ FileHandlingExample6/FileHandlingExample6/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileHandlingExample6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string data;
+            FileStream fsSource = new FileStream("E:\\csharpfile.txt", FileMode.Open, FileAccess.Read);
+            using (StreamReader sr = new StreamReader(fsSource))
+            {
+                data = sr.ReadToEnd();
+            }
+            Console.WriteLine(data);
+            Console.ReadKey();
+        }
+    }
+}
