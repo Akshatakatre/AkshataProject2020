98ef7ae60f0f0ef936329affce7773c361278c65
Akshata Katre <akshataa2sk@gmail.com>
2020-09-25 15:25:15 +0530

    practice program

---
+++ FileHandlingExample3/FileHandlingExample3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace FileHandlingExample3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String path = @"E:\Example.txt";
+
+            String[] lines;
+            lines = File.ReadAllLines(path);
+
+            Console.WriteLine(lines[0]);
+            Console.WriteLine(lines[1]);
+
+            Console.ReadKey();
+        }
+    }
+}
