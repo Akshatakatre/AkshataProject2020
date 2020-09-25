c36411d78a4e201b0eea0e431253783079b40f84
Akshata Katre <akshataa2sk@gmail.com>
2020-09-25 15:23:54 +0530

    practice program

---
+++ FileHandlingExample2/FileHandlingExample2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace FileHandlingExample2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (File.Exists("test.txt"))
+            {
+                string content = File.ReadAllText("test.txt");
+                Console.WriteLine("Current content of file:");
+                Console.WriteLine(content);
+            }
+            Console.WriteLine("Please enter new content for the file:");
+            string newContent = Console.ReadLine();
+            File.WriteAllText("test.txt", newContent);
+            Console.ReadKey();
+        }
+    }
+}
