97170ab927aefc45682da213b51484e4d34c39ea
Akshata Katre <akshataa2sk@gmail.com>
2020-09-26 15:40:52 +0530

    practice program

---
+++ Constructor3/Constructor3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Constructor3
+{
+    class Program
+    {
+        public Program()
+        {
+            Console.WriteLine("This is our Default Constructor");
+            Console.WriteLine("-----------------------------------------");
+        }
+        public Program(string name)
+        {
+            Console.WriteLine("This is our Parametrized Constructor");
+            Console.WriteLine("Name is :" + name);
+        }
+        public Program(string name, int age)
+        {
+            Console.WriteLine("Name is " + name);
+            Console.WriteLine("Age is " + age);
+        }
+        static void Main(string[] args)
+        {
+            Program s = new Program();
+            Program s1 = new Program("Akshata");
+            Program s2 = new Program("Akshu",24);
+            Console.ReadKey();
+        }
+    }
+}
