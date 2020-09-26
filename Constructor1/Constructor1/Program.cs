e27748857807e2b6603e67c4a5b0f1aa0f0fcbe7
Akshata Katre <akshataa2sk@gmail.com>
2020-09-26 15:24:13 +0530

    practice programs

---
+++ Constructor1/Constructor1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Constructor1
+{
+   public class Program
+    {
+        private Program()
+        {
+        }
+
+        public static int count_number;
+
+        public static int Count()
+        {
+            return ++count_number;
+        }
+        static void Main(string[] args)
+        {
+            Program.count_number = 26;
+            Program.Count();
+
+            Console.WriteLine(Program.count_number);
+
+            Program.Count();
+
+            Console.WriteLine(Program.count_number);
+            Console.ReadKey();
+        }
+    }
+}
