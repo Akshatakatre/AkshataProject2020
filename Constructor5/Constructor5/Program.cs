425dcf732b410d3c7aecaf2a2260318a334f2d23
Akshata Katre <akshataa2sk@gmail.com>
2020-09-26 15:45:32 +0530

    practice program

---
+++ Constructor5/Constructor5/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Constructor5
+{
+    class Program
+    {
+        public string Name { get; set; }
+        public int num1 { get; set; }
+        public int num2 { get; set; }
+        public int sum { get; set; }
+
+        public Program()
+        {
+            Console.WriteLine("This is parameterless constructor");
+        }
+        public Program(string n)
+        {
+            Name = n;
+            Console.WriteLine("Name is : " + n);
+        }
+        public Program(int a,int b)
+        {
+            num1 = a;
+            num2 = b;
+            sum = num1 + num2;
+            Console.WriteLine("Addition of two number is : " + sum);
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            Program p1 = new Program("Tom&Jerry");
+            Program p2 = new Program(26, 2);
+            Console.ReadKey();
+
+        }
+    }
+}
