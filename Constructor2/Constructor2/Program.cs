5f7ee6bee3ec87586267a20ab35e4ca4a877808a
Akshata Katre <akshataa2sk@gmail.com>
2020-09-26 15:39:06 +0530

    practice program

---
+++ Constructor2/Constructor2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Constructor2
+{
+    class Program
+    {
+        static Program()
+        {
+
+            Console.WriteLine("Static Constructor");
+        }
+ 
+        public Program(int i)
+        {
+            Console.WriteLine("Instance Constructor " + i);
+        }
+
+        public string detail(string name, int id)
+        {
+            return "Name:" + name + " id:" + id;
+        }
+        static void Main(string[] args)
+        {
+            Program obj = new Program(1);
+
+            Console.WriteLine(obj.detail("Tom", 1));
+
+            Program obj1 = new Program(2);
+
+            Console.WriteLine(obj1.detail("Jerry", 2));
+            Console.ReadKey();
+        }
+    }
+}
