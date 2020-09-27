e9455b570b50c00a2b180786ec9ca9fb3bd97ee2
Akshata Katre <akshataa2sk@gmail.com>
2020-09-27 15:07:49 +0530

    practice program

---
+++ GenericDictionaryExample/GenericDictionaryExample/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericDictionaryExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declare Generic Collection
+            Dictionary<string, string> movie = new Dictionary<string, string>();
+
+            //Adding Item in Collection
+            movie.Add("sci-fi", "Star Wars");
+            movie.Add("Action", "The One");
+            movie.Add("Fantacy", "Star Dust");
+            movie.Add("Computer", "Algorithm");
+            movie.Add("Horror", "Anabella");
+
+            //Printing single item
+            Console.WriteLine(movie["sci-fi"]);
+
+            //Iterate Dictionary
+            foreach (KeyValuePair<string, string> items in movie)
+            {
+                Console.WriteLine(items.ToString());
+            }
+
+            //Find items
+            if (movie.ContainsKey("sci-fi"))
+            {
+                Console.WriteLine("Key found. Value is " + movie["sci-fi"]);
+            }
+            else
+            {
+                Console.WriteLine("Key Not Found");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
