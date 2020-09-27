5a68c1de2df4480c85ab0f0cfea527aec089ffdf
Akshata Katre <akshataa2sk@gmail.com>
2020-09-27 15:01:03 +0530

    practice program

---
+++ PolymorphismExample1/PolymorphismExample1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace PolymorphismExample1
+{
+    class Program
+    {
+        void print(int i)
+        {
+            Console.WriteLine("Printing int: {0}", i);
+        }
+        void print(double f)
+        {
+            Console.WriteLine("Printing float: {0}", f);
+        }
+        void print(string s)
+        {
+            Console.WriteLine("Printing string: {0}", s);
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+
+            // Call print to print integer
+            p.print(5);
+
+            // Call print to print float
+            p.print(500.263);
+
+            // Call print to print string
+            p.print("Hello and Welcome...!");
+            Console.ReadKey();
+        }
+    }
+}
