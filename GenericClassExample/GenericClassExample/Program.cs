fc6522f0d0112232fd0e3a3a62f4b4fcf85c55a4
Akshata Katre <akshataa2sk@gmail.com>
2020-09-27 15:06:30 +0530

    practice program

---
+++ GenericClassExample/GenericClassExample/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericClassExample
+{
+    //Declare Generics 
+    public class GenericClass<T>
+    {
+        public void GenFunction(T printvalue)
+        {
+            Console.WriteLine(printvalue);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Printing Integer Value");
+            GenericClass<int> gen = new GenericClass<int>();
+            gen.GenFunction(304);
+
+            Console.WriteLine("Printing String Value");
+            GenericClass<string> genstring = new GenericClass<string>();
+            genstring.GenFunction("Hello.... and Welcome.....");
+            Console.ReadKey();
+        }
+    }
+}
