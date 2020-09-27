9b001f204e6cb3046d4342bc6c5aa98d89c2cca8
Akshata Katre <akshataa2sk@gmail.com>
2020-09-27 15:04:50 +0530

    practice program

---
+++ PolymorphismExample4/PolymorphismExample4/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace PolymorphismExample4
+{
+    class Animal  
+    {
+        public void animalSound()
+        {
+            Console.WriteLine("The animal makes a sound");
+        }
+    }
+
+    class Cat : Animal   
+    {
+        public void animalSound()
+        {
+            Console.WriteLine("The cat says: Mewo Mewo");
+        }
+    }
+
+    class Dog : Animal  
+    {
+        public void animalSound()
+        {
+            Console.WriteLine("The dog says: bow wow");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal myAnimal = new Animal();  
+            Animal myCat = new Cat(); 
+            Animal myDog = new Dog();
+            Cat c = new Cat();
+            Dog d = new Dog();
+
+            myAnimal.animalSound();
+            //myCat.animalSound();
+            //myDog.animalSound();
+            c.animalSound();
+            d.animalSound();
+            Console.ReadKey();
+        }
+    }
+}
