4af03d394b07272b05ddbcff9b23ce5b3bd9fe8b
Akshata Katre <akshataa2sk@gmail.com>
2020-09-30 12:33:47 +0530

    practice program

---
+++ PolymorphismExample10/PolymorphismExample10/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace PolymorphismExample10
+{
+    public class A
+    {
+        public virtual void GetInfo()
+        {
+            Console.WriteLine("Base Class A Method");
+        }
+        public virtual void Test()
+        {
+            Console.WriteLine("Base Class A Test Method");
+        }
+    }
+    public class B : A
+    {
+        public sealed override void GetInfo()
+        {
+            Console.WriteLine("Derived Class B Method");
+        }
+        public override void Test()
+        {
+            Console.WriteLine("Derived Class B Test Method");
+        }
+
+    }
+
+    public class C : B
+    {
+       /* public override void GetInfo()
+        {
+            //Console.WriteLine("Age: {0}", base.age);
+        }*/
+        public override void Test()
+        {
+            Console.WriteLine("Derived Class C Test Method");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            C c = new C();
+            c.GetInfo();
+            c.Test();
+            Console.ReadKey();
+        }
+    }
+}
