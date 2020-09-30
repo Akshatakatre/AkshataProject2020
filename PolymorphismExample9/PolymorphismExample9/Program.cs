b03e6314f69697d60c90eb260c0d306ed8844598
Akshata Katre <akshataa2sk@gmail.com>
2020-09-30 12:32:01 +0530

    practice program

---
+++ PolymorphismExample9/PolymorphismExample9/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace PolymorphismExample9
+{
+    public class BClass
+    {
+        public virtual void GetInfo()
+        {
+            Console.WriteLine("Hello... This is base class method.");
+        }
+    }
+    public class DClass : BClass
+    {
+        public override void GetInfo()
+        {
+            Console.WriteLine("Hello.. this is derived class method.");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DClass d = new DClass();
+            d.GetInfo();
+            BClass b = new BClass();
+            b.GetInfo();
+            Console.ReadKey();
+        }
+    }
+}
