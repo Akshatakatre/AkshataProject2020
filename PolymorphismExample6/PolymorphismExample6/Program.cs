bf0aad9c54b1c0c0d8f736b0f6221e9ad9456800
Akshata Katre <akshataa2sk@gmail.com>
2020-09-30 12:27:29 +0530

    practice program

---
+++ PolymorphismExample6/PolymorphismExample6/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace PolymorphismExample6
+{
+    class Program
+    {
+        public class Print
+        {
+            public void display(string name)
+            {
+                Console.WriteLine("Your name is : " + name);
+            }
+            public void display(int age, float marks)
+            {
+                Console.WriteLine("Your age is : " + age);
+                Console.WriteLine("Your marks are :" + marks);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Print obj = new Print();
+            obj.display("Akshata");
+            obj.display(24, 76.50f);
+            Console.ReadKey();
+        }
+    }
+}
