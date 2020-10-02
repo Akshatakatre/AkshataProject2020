b5073ac202c8c1d53501cd8958dd63ff9fa3c828
Akshata Katre <akshataa2sk@gmail.com>
2020-10-02 13:54:31 +0530

    practice program

---
+++ InterfaceExample6/InterfaceExample6/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace InterfaceExample6
+{
+    interface IAnimal
+    {
+        void animalSound(); 
+    }
+
+    class Cat : IAnimal
+    {
+        public void animalSound()
+        {
+            
+            Console.WriteLine("The cat says: meow... meow...");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat myCat = new Cat();  
+            myCat.animalSound();
+            Console.ReadKey();
+        }
+    }
+}
