ae06e83dcfbbfd5998a3fa23f098dbb468e86b0c
Akshata Katre <akshataa2sk@gmail.com>
2020-10-02 13:48:59 +0530

    practice program

---
+++ InterfaceExample1/InterfaceExample1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace InterfaceExample1
+{
+    interface IAccount
+    {
+        string AccountInfo();
+        void process();
+    }
+
+    class SavingAccount : IAccount
+    {
+
+        public string AccountInfo()
+        {
+            return "Saving Account";
+        }
+        public void process()
+        {
+            Console.WriteLine("Process saving account");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SavingAccount account = new SavingAccount();
+            Console.WriteLine(account.AccountInfo());
+            account.process();
+            Console.ReadKey();
+        }
+    }
+}
