5d4376315cf33780eb25706c0f4402ecb3322574
Akshata Katre <akshataa2sk@gmail.com>
2020-10-02 13:50:05 +0530

    practice program

---
+++ InterfaceExample2/InterfaceExample2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace InterfaceExample2
+{
+    interface ITransaction
+    {
+        void doTransaction();
+    }
+    class ConnectDatabase
+    {
+        public void connect()
+        {
+            Console.WriteLine("Connect database");
+        }
+    }
+
+    class Transaction : ConnectDatabase, ITransaction
+    {
+        public void doTransaction()
+        {
+            Console.WriteLine("processing transaction");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Transaction t = new Transaction();
+            t.connect();
+            t.doTransaction();
+            Console.ReadKey();
+        }
+    }
+}
