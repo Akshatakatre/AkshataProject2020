8d79f1b6de5003dabb8e43c3668b091fa704e8dc
Akshata Katre <akshataa2sk@gmail.com>
2020-10-02 13:55:31 +0530

    practice program

---
+++ InterfaceExample7/InterfaceExample7/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace InterfaceExample7
+{
+    interface IFirstInterface
+    {
+        void myMethod(); 
+    }
+
+    interface ISecondInterface
+    {
+        void myOtherMethod(); 
+    }
+
+    class DemoClass : IFirstInterface, ISecondInterface
+    {
+        public void myMethod()
+        {
+            Console.WriteLine("Hello.... ");
+        }
+        public void myOtherMethod()
+        {
+            Console.WriteLine("How are you..");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DemoClass myObj = new DemoClass();
+            myObj.myMethod();
+            myObj.myOtherMethod();
+            Console.ReadKey();
+        }
+    }
+}
