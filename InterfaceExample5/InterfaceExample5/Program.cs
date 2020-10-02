3ef7a1dadb5729ef4901d22995ea563b5a1cf6b6
Akshata Katre <akshataa2sk@gmail.com>
2020-10-02 13:53:28 +0530

    practice program

---
+++ InterfaceExample5/InterfaceExample5/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace InterfaceExample5
+{
+    interface IStudent
+    {
+        string Name
+        {
+            get;
+            set;
+        }
+        void study();
+    }
+
+    public class Student : IStudent
+    {
+        private string name;
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+        public void study()
+        {
+            Console.WriteLine("Study");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s = new Student();
+            Console.Write("Enter Student name: ");
+            s.Name = System.Console.ReadLine();
+            Console.WriteLine("Student name: {0}", s.Name);
+            s.study();
+            Console.ReadKey();
+        }
+    }
+}
