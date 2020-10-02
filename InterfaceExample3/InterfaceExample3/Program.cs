0477bdf0c92bf9a61c124d606f7989bfe6ec113b
Akshata Katre <akshataa2sk@gmail.com>
2020-10-02 13:51:19 +0530

    practice program

---
+++ InterfaceExample3/InterfaceExample3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace InterfaceExample3
+{
+    interface IFlyable
+    {
+        void fly();
+    }
+    interface IEatable
+    {
+        void eat();
+    }
+
+    class Parrot : IFlyable, IEatable
+    {
+
+        public void eat()
+        {
+            Console.WriteLine("Parrot eating");
+        }
+        //Implement method of interface
+        public void fly()
+        {
+            Console.WriteLine("Parrot flying");
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Parrot p = new Parrot();
+            p.eat();
+            p.fly();
+            Console.ReadKey();
+        }
+    }
+}
