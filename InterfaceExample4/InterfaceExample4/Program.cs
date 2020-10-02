16b9f57994e375956ee5682c91941d662f3887fc
Akshata Katre <akshataa2sk@gmail.com>
2020-10-02 13:52:26 +0530

    practice program

---
+++ InterfaceExample4/InterfaceExample4/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace InterfaceExample4
+{
+    interface I1st
+    {
+
+        void rank();
+
+    }
+
+    interface I2nd
+    {
+
+        void rank();
+
+    }
+
+    class Position : I1st, I2nd
+    {
+
+        void I1st.rank()
+        {
+            Console.WriteLine("Rank 1st");
+        }
+
+        void I2nd.rank()
+        {
+            Console.WriteLine("Rank 2nd");
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            I1st p = new Position();
+            p.rank();
+            Console.ReadKey();
+            
+        }
+    }
+}
