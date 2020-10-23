66b2fc97b86f917cef9ed6543ac1b1d930ad7ffc
Akshata Katre <akshataa2sk@gmail.com>
2020-10-23 16:26:53 +0530

    practice program

---
+++ IfElseStatement8/IfElseStatement8/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace IfElseStatement8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = -1;
+            int b = -1;
+            //check the error will occur or not
+            if (Convert.ToBoolean(++a = ++b))
+                Console.WriteLine("a");
+            else
+                Console.WriteLine("b");
+            Console.ReadKey();
+
+        }
+    }
+}
