5207439a2c7e1758e27ac92c22f2f05f587fcc9f
Akshata Katre <akshataa2sk@gmail.com>
2020-10-23 16:13:53 +0530

    practice program

---
+++ ForLoopExample3/ForLoopExample3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ForLoopExample3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int I, X;
+            for (I = 1; I <= (9 % 2 + I); I++)
+            {
+                X = (I * 3 + I * 2) / I;
+                Console.WriteLine(X);
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
