bf420217cf2353cb7022ecae375ac047845ae9e3
Akshata Katre <akshataa2sk@gmail.com>
2020-10-23 16:14:57 +0530

    practice program

---
+++ ForLoopExample4/ForLoopExample4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ForLoopExample4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int I, J = 0;
+            for (I = 1; I < 10;) ;
+            {
+                J = J + I;
+                I += 2;
+            }
+            Console.WriteLine("Sum of first 10 even numbers is:" + J);
+            Console.ReadKey();
+
+        }
+    }
+}
