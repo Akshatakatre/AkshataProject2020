f78d1772a46d1b6605cf03ad68cd8f0a3691cce3
Akshata Katre <akshataa2sk@gmail.com>
2020-10-23 16:27:57 +0530

    practice program

---
+++ IfElseStatement9/IfElseStatement9/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace IfElseStatement9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5, b = 10;
+            if (Convert.ToBoolean(Convert.ToInt32(0xB)))
+                if (Convert.ToBoolean(Convert.ToInt32(022)))
+                    if (Convert.ToBoolean(Convert.ToInt32('\xeb')))
+                        Console.WriteLine("java");
+                    else;
+                else;
+            else;
+            Console.ReadKey();
+        }
+    }
+}
