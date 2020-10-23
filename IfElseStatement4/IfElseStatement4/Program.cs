5ca603df51b4d4166da5b2f535eb7d180520cfc2
Akshata Katre <akshataa2sk@gmail.com>
2020-10-23 16:23:34 +0530

    practice program

---
+++ IfElseStatement4/IfElseStatement4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace IfElseStatement4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j;
+            for (i = 2; i >= 0; i--)
+            {
+                for (j = 0; j <= 2; j++)
+                {
+                    if (i == j)
+                    {
+                        Console.WriteLine("1");
+                    }
+                    else
+                    {
+                        Console.WriteLine("0");
+                    }
+                }
+                Console.WriteLine("\n");
+                Console.ReadKey();
+
+            }
+        }
+    }
+}
