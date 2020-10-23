acf655d794626602afc5bba97fd92ccdbef15b0b
Akshata Katre <akshataa2sk@gmail.com>
2020-10-23 16:18:57 +0530

    practice program

---
+++ ForLoopExample9/ForLoopExample9/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ForLoopExample9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            for (i = -3; i <= 3; i++)
+            {
+                switch (i)
+                {
+                    case 0:
+                        Console.WriteLine("zero");
+                        break;
+                }
+                if (i > 0)
+                    Console.WriteLine("A");
+                else if (i < 0)
+                    Console.WriteLine("B");
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
