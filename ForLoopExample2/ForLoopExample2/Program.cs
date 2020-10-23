678d264bc0bb04009492e133c3b62ff683352528
Akshata Katre <akshataa2sk@gmail.com>
2020-10-23 16:12:51 +0530

    practice program

---
+++ ForLoopExample2/ForLoopExample2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ForLoopExample2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            float f;
+            for (f = 0.1f; f <= 0.5; f += 1)
+                Console.WriteLine(++f);
+            Console.ReadKey();
+
+        }
+    }
+}
