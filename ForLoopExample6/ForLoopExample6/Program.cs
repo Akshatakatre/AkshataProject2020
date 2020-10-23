6dd42e5b790b2e89bd8337f0786d6e6cb9a4104b
Akshata Katre <akshataa2sk@gmail.com>
2020-10-23 16:16:57 +0530

    practice program

---
+++ ForLoopExample6/ForLoopExample6/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ForLoopExample6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, s = 0;
+            for (i = 1; i <= 10; s = s + i, i++) ;
+            {
+                Console.WriteLine(s);
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
