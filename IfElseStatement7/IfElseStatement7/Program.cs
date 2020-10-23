1b9fe7373e840921964ccec549bd01db03a5ebe8
Akshata Katre <akshataa2sk@gmail.com>
2020-10-23 16:26:01 +0530

    practice program

---
+++ IfElseStatement7/IfElseStatement7/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace IfElseStatement7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            if (Convert.ToBoolean((.002f) - (0.1f)))
+                Console.WriteLine("Sachin Tendulkar");
+            else if (a == 5)
+                Console.WriteLine("Rahul Dravid");
+            else
+                Console.WriteLine("Ms Dhoni");
+            Console.ReadKey();
+
+        }
+    }
+}
