10d0f909dcf496947fae18d3455ca2b971fab8cd
Akshata Katre <akshataa2sk@gmail.com>
2020-10-23 16:11:01 +0530

    practice program

---
+++ ForLoopExample10/ForLoopExample10/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ForLoopExample10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j;
+            for (i = 1, j = i; i <= 3 && j >= 0; i++, j--)
+            {
+                if (i == j)
+                    continue;
+                else
+                    Console.WriteLine(j);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
