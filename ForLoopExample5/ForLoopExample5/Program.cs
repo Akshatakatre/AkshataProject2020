b13250b2c8428d9c0da3766a5354821785901ec1
Akshata Katre <akshataa2sk@gmail.com>
2020-10-23 16:16:00 +0530

    practice program

---
+++ ForLoopExample5/ForLoopExample5/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ForLoopExample5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 5;
+            for (; Convert.ToBoolean(Convert.ToInt32(i)); Console.WriteLine(i--)) ;
+            Console.ReadKey();
+
+        }
+    }
+}
