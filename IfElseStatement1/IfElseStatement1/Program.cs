6efe023a645508746649a019338f8baed105adb8
Akshata Katre <akshataa2sk@gmail.com>
2020-10-23 16:19:52 +0530

    practice program

---
+++ IfElseStatement1/IfElseStatement1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace IfElseStatement1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 30;
+            int j = 25 % 25;
+            if (Convert.ToBoolean(Convert.ToInt32(i = j)))
+            {
+                Console.WriteLine("In if");
+            }
+            else
+            {
+                Console.WriteLine("In else");
+            }
+            Console.WriteLine("In main");
+            Console.ReadKey();
+
+        }
+    }
+}
