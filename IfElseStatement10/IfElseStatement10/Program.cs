7f94dfca18f2b85cad1479f80290a7be07f3e04f
Akshata Katre <akshataa2sk@gmail.com>
2020-10-23 16:20:54 +0530

    practice program

---
+++ IfElseStatement10/IfElseStatement10/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace IfElseStatement10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5, b = 10;
+            if (Convert.ToBoolean(Convert.ToInt32(++a)) || Convert.ToBoolean(Convert.ToInt32(++b)))
+            {
+                Console.WriteLine(a + "\n" + b);
+            }
+            else
+                Console.WriteLine(" C# ");
+            Console.ReadKey();
+        }
+    }
+}
