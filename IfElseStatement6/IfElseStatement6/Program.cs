c60579ffb6b0fbe8c584410eb0050313c4fd848a
Akshata Katre <akshataa2sk@gmail.com>
2020-10-23 16:25:09 +0530

    practice program

---
+++ IfElseStatement6/IfElseStatement6/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace IfElseStatement6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 15, b = 10, c = 1;
+            if (Convert.ToBoolean(a) && (b > c))
+            {
+                Console.WriteLine("cquestionbank");
+            }
+            else
+            {
+                //check the error will occor or not
+                break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
