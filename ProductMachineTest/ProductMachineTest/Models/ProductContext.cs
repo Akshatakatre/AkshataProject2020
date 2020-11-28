550b8970b83c4d0bcc1f47baf69dbd5e6fdf1f5a
Akshata Katre <akshataa2sk@gmail.com>
2020-11-28 13:38:48 +0530

    Machine test

---
+++ ProductMachineTest/ProductMachineTest/Models/ProductContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProductMachineTest.Models
+{
+    public class ProductContext : DbContext
+    {
+        public ProductContext(DbContextOptions<ProductContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ProductItem> ProductItems { get; set; }
+
+    }
+}
---
+++ ProductMachineTest/ProductMachineTest/Models/ProductItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductMachineTest.Models
+{
+    public class ProductItem
+    {
+        public long Id { get; set; }
+        public string ProductName { get; set; }
+        public string CompanyName { get; set; }
+        public string ProductId { get; set; }
+        public float price { get; set; }
+        public string Quantity { get; set; }
+        public bool IsComplete { get; set; }
+
+    }
+}
