53e4e62a273b9adab60cde5a728cd6d48a3cde79
Akshata Katre <akshataa2sk@gmail.com>
2020-12-02 13:23:52 +0530

    practice program

---
+++ ConsumingRestAPI/ConsumingRestAPI/Models/DataManager/EmployeeManager.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ConsumingRestAPI.Models.Repository;
+
+namespace ConsumingRestAPI.Models.DataManager
+{
+    public class EmployeeManager : IDataRepository<Empolyee>
+    {
+        readonly EmployeeContext _employeeContext;
+        private EmployeeContext empolyee;
+
+        public EmployeeManager(EmployeeContext context)
+        {
+            _employeeContext = context;
+        }
+        public IEnumerable<Empolyee> GetAll()
+        {
+            return _employeeContext.Employee.ToList();
+        }
+        public Empolyee Get(long id)
+        {
+            return _employeeContext.Employee
+                  .FirstOrDefault(e => e.EmployeeId == id);
+        }
+        public void Add(Empolyee entity)
+        {
+            _employeeContext.Employee.Add(entity);
+            _employeeContext.SaveChanges();
+        }
+        public void Update(Empolyee employee, Empolyee entity)
+        {
+            employee.FirstName = entity.FirstName;
+            employee.LastName = entity.LastName;
+            employee.Email = entity.Email;
+            employee.DateOfBirth = entity.DateOfBirth;
+            employee.PhoneNumber = entity.PhoneNumber;
+            _employeeContext.SaveChanges();
+        }
+        public void Delete(Empolyee employee)
+        {
+            _employeeContext.Employees.Remove(empolyee);
+            _employeeContext.SaveChanges();
+        }
+
+
+
+       
+    }
+}
---
+++ ConsumingRestAPI/ConsumingRestAPI/Models/EmployeeContext.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ConsumingRestAPI.Models
+{
+    public class EmployeeContext : DbContext
+    {
+        public EmployeeContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+        public DbSet<EmployeeContext> Employees { get; set; }
+        public object Employee { get; internal set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Empolyee>().HasData(new Empolyee
+            {
+                EmployeeId = 1,
+                FirstName = "Uncle",
+                LastName = "Bob",
+                Email = "uncle.bob@gmail.com",
+                DateOfBirth = new DateTime(1979, 04, 25),
+                PhoneNumber = "999-888-7777"
+            }, new Empolyee
+            {
+                EmployeeId = 2,
+                FirstName = "Jan",
+                LastName = "Kirsten",
+                Email = "jan.kirsten@gmail.com",
+                DateOfBirth = new DateTime(1981, 07, 13),
+                PhoneNumber = "111-222-3333"
+            });
+
+        }
+    }
+
+
+}
