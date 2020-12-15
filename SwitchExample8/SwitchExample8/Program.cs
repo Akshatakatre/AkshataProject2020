22bc88b0e4fd889ef7f5f9ccd58eb87e7d9f0a25
Akshata Katre <akshataa2sk@gmail.com>
2020-12-15 22:47:31 +0530

    practice program

---
+++ SwitchExample8/SwitchExample8/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace SwitchExample8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char ch = Convert.ToChar('a' | 'b' | 'c');
+            switch (ch)
+            {
+                case 'A':
+                case 'a':
+                    Console.WriteLine("case A|case a");
+                    break;
+                case 'B':
+                case 'b':
+                    Console.WriteLine("case B|case b");
+                    break;
+                case 'C':
+                case 'c':
+                case 'D':
+                case 'd':
+                    Console.WriteLine("case D|case d");
+                    break;
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
