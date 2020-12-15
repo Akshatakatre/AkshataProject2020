826caa111c882b27772ebec107554926b189960e
Akshata Katre <akshataa2sk@gmail.com>
2020-12-15 22:42:58 +0530

    practice program

---
+++ SwitchExample6/SwitchExample6/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace SwitchExample6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            switch (5)
+            {
+                case 5.0f:
+                    Console.WriteLine("harsh");
+                    break;
+                case 5:
+                    Console.WriteLine("amish");
+                    break;
+                case 5.0L:
+                    Console.WriteLine("ANKIT");
+                    break;
+                default:
+                    Console.WriteLine("ashish");
+            }
+            Console.ReadLine();
+            //error occur
+        }
+    }
+}
