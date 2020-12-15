2f353387fcf0ac21dc61d0ba2df34a013e84bb07
Akshata Katre <akshataa2sk@gmail.com>
2020-12-15 22:34:24 +0530

    practice program

---
+++ SwitchExample3/SwitchExample3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace SwitchExample3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int const p = 0;
+            switch (3 * 5 / 6)
+            {
+                case p:
+                    Console.WriteLine("A");
+                    break;
+                case p * 1:
+                    Console.WriteLine("B");
+                    break;
+                case p - 2:
+                    Console.WriteLine("C");
+                    break;
+                default:
+                    Console.WriteLine("D");
+            }
+            //error occur in this code
+        }
+    }
+}
