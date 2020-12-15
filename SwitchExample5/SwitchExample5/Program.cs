6d962e5314e738d912f441c0aff70ac2cd1b260e
Akshata Katre <akshataa2sk@gmail.com>
2020-12-15 22:40:30 +0530

    practice program

---
+++ SwitchExample5/SwitchExample5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SwitchExample5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 9, j = 7;
+            switch (i - j + 3)
+            {
+                case 9:
+                    7:
+          j += 6;
+                    break;
+                case 5:
+                    i -= 4;
+                    break;
+            }
+            Console.WriteLine(i + "\n" + j);
+            Console.ReadKey();
+            //compile time error occur
+        }
+    }
+}
