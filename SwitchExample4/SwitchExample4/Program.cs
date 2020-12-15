ce0bad34968217bb932bd6f1380a4b4c1090d018
Akshata Katre <akshataa2sk@gmail.com>
2020-12-15 22:36:58 +0530

    practice program

---
+++ SwitchExample4/SwitchExample4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace SwitchExample4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 2, j = 3, k = 4;
+            switch (i + j - k)
+            {
+                case 0:
+                case 2:
+                case 4:
+                    ++i;
+                    k += j;
+                    break;
+                case 1:
+                case 3:
+                case 5:
+                    --i;
+                    k -= j;
+                    break;
+                default:
+                    i += j;
+                    break;
+            }
+            Console.WriteLine(i + "\n" + j + "\n" + k);
+            Console.ReadKey();
+
+        }
+    }
+}
