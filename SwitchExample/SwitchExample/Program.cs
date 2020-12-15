afa2d43a6d8d895801b433de8a85a492248e77e5
Akshata Katre <akshataa2sk@gmail.com>
2020-12-15 22:24:12 +0530

    practice program

---
+++ SwitchExample/SwitchExample/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SwitchExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int movie = 1;
+            switch (movie << 2 + movie)
+            {
+                default:
+                    Console.WriteLine("3 Idiots");
+                    break;
+                case 4:
+                    Console.WriteLine("Ghazini");
+                    break;
+                case 5:
+                    Console.WriteLine("Krishh");
+                    break;
+                case 8:
+                    Console.WriteLine("Race");
+                    break;
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
