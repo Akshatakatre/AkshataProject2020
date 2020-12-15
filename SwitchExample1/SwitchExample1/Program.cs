48392961ef5cbd3493ed89492ff9a0287fb12a7a
Akshata Katre <akshataa2sk@gmail.com>
2020-12-15 22:27:01 +0530

    practice program

---
+++ SwitchExample1/SwitchExample1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SwitchExample1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 2, j = 4;
+            switch (i + j * 2)
+            {
+                case 1:
+                case 2:
+                    Console.WriteLine("1 and 2");
+                    break;
+                    //error occur in this code
+                case 3 to 10:
+          Console.WriteLine("3 to 10");
+                    break;
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
