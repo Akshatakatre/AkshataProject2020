d79513e1c014c243059b70c746b10ca2e9295e7d
Akshata Katre <akshataa2sk@gmail.com>
2020-12-15 22:50:33 +0530

    practice program

---
+++ SwitchExample9/SwitchExample9/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SwitchExample9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char ch = 'p';
+            switch (ch)
+            {
+                case 'p':
+                    Console.WriteLine("coco" + "\t" + Convert.ToInt32(ch));
+                    break;
+                default:
+                    Console.WriteLine("default");
+                    break;
+            }
+            Console.WriteLine("main");
+            Console.ReadKey();
+        }
+    }
+}
