0193005bd44d33d953a0f4d36132fdd7512a1ac0
Akshata Katre <akshataa2sk@gmail.com>
2020-12-16 16:11:40 +0530

    practice program

---
+++ WhileLoop8/WhileLoop8/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace WhileLoop8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            while (i <= 50)
+            {
+                if (i % 10 == 0)
+                    continue;
+                else
+                    break;
+                i += 10;
+                Console.WriteLine(i % 10);
+            }
+          //run infinite loop but doesnt print any thing.
+
+        }
+    }
+}
