a15517ab62b45cb54e9f0b275ff7ccc3877e5c5d
Akshata Katre <akshataa2sk@gmail.com>
2020-12-16 16:08:01 +0530

    practice program

---
+++ WhileLoop6/WhileLoop6/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace WhileLoop6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, r;
+            n = Convert.ToInt32(Console.ReadLine());
+            while (n > 0)
+            {
+                r = n % 10;
+                n = n / 10;
+                Console.WriteLine(+r);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
