9cc0064ae9c096448b30fdab3f7ef790bb4930f3
Akshata Katre <akshataa2sk@gmail.com>
2020-12-16 15:54:15 +0530

    practice program

---
+++ WhileLoop10/WhileLoop10/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace WhileLoop10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            while (i <= 1)
+            {
+                if ('A' < 'a')
+                {
+                    Console.WriteLine("Hello...");
+                }
+                else
+                {
+                    Console.WriteLine("Hi...");
+                }
+                i++;
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
