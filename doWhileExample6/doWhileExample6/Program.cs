e075eb741cf67c34abe714754f9d63ab07663f9b
Akshata Katre <akshataa2sk@gmail.com>
2020-12-16 15:20:08 +0530

    practice program

---
+++ doWhileExample6/doWhileExample6/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace doWhileExample6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, s = 0, a = 1, d;
+            i = Convert.ToInt32(Console.ReadLine());
+            do
+            {
+                d = i % (2 * 4);
+                s = s + d * a;
+            } while ((Convert.ToInt32(i = i / (2 * 4))) != 0
+             && (Convert.ToBoolean(Convert.ToInt32((a) = (a * 10)))));
+            Console.WriteLine(s);
+            Console.ReadLine();
+
+        }
+    }
+}
