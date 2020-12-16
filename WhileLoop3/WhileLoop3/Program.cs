9825f18e09a239d9758c9caa85751e81c9aae27e
Akshata Katre <akshataa2sk@gmail.com>
2020-12-16 16:02:44 +0530

    practice program

---
+++ WhileLoop3/WhileLoop3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace WhileLoop3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            i = 0;
+            while (i++ < 5)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("\n");
+            i = 0;
+            while (++i < 5)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
