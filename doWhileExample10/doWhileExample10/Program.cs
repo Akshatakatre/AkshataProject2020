971a035edf64da42ee9457887a6e1a299b019472
Akshata Katre <akshataa2sk@gmail.com>
2020-12-16 15:12:53 +0530

    practice program

---
+++ doWhileExample10/doWhileExample10/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace doWhileExample10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            for (x = 1; x <= 3; x++)
+            {
+                int j = 1;
+                do
+                {
+                    j++;
+                } while (x % j == 2);
+                Console.WriteLine(x + " " + j);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
