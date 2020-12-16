372a2c9fff10bf98a62a262391feb0c5bb5ffea9
Akshata Katre <akshataa2sk@gmail.com>
2020-12-16 16:06:24 +0530

    practice program

---
+++ WhileLoop5/WhileLoop5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace WhileLoop5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            x = Convert.ToInt32(Console.ReadLine());
+            int c = 1;
+            while (c <= x)
+            {
+                if (c % 2 == 0)
+                {
+                    Console.WriteLine("Execute While " + c + "\t" + "time");
+                }
+                c++;
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
