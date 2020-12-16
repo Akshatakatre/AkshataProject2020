c88dc62c9d65e5e709ddc6478e135903516df3b8
Akshata Katre <akshataa2sk@gmail.com>
2020-12-16 14:36:07 +0530

    practice program

---
+++ doWhileExampe3/doWhileExampe3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace doWhileExampe3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1, j = 5;
+            do
+            {
+                Console.WriteLine(i = i++ * j);
+            } while (i <= 10);
+            Console.ReadLine();
+
+        }
+    }
+}
