382b2deb2184bb1b862750b913cf8f5aed1bb2c0
Akshata Katre <akshataa2sk@gmail.com>
2020-12-16 15:23:43 +0530

    practice program

---
+++ doWhileExample7/doWhileExample7/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace doWhileExample7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10;
+            do
+            {
+                Console.WriteLine(x++);
+            }
+            while (Convert.ToBoolean(5) && Convert.ToBoolean(4) && Convert.ToBoolean(3)
+            && Convert.ToBoolean(2) && Convert.ToBoolean(1) && Convert.ToBoolean(0));
+            Console.ReadLine();
+
+        }
+    }
+}
