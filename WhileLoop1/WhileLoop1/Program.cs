d802f74a64902f8ea3d2cad9edf1ee85685c7b93
Akshata Katre <akshataa2sk@gmail.com>
2020-12-16 15:52:33 +0530

    practice program

---
+++ WhileLoop1/WhileLoop1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace WhileLoop1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j;
+            for (i = 1; i <= 3; i++)
+            {
+                j = 1;
+                while (i % j == 2)
+                {
+                    j++;
+                }
+                Console.WriteLine(i + " " + j);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
