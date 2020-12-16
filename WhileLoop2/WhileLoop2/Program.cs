d61e8b8e2f47df0794a19229d699d0025d5371f4
Akshata Katre <akshataa2sk@gmail.com>
2020-12-16 15:58:09 +0530

    practice program

---
+++ WhileLoop2/WhileLoop2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace WhileLoop2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float s = 0.1f;
+            while (s <= 0.5f)
+            {
+                ++s;
+                Console.WriteLine(s);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
