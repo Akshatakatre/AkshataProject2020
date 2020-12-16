c60d4e9265431b222f8e0319eff1e3ac8a11c7a4
Akshata Katre <akshataa2sk@gmail.com>
2020-12-16 15:18:20 +0530

    practice program

---
+++ doWhileExample5/doWhileExample5/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace doWhileExample5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long x;
+            x = Convert.ToInt32(Console.ReadLine());
+            do
+            {
+                Console.WriteLine(x % 10);
+            } while ((x = x / 10) != 0);
+            Console.ReadLine();
+
+        }
+    }
+}
