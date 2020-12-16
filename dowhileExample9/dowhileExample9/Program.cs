6ff3853c4c5d7d3dacda13fa2e3f6dda41f6a8dd
Akshata Katre <akshataa2sk@gmail.com>
2020-12-16 15:25:30 +0530

    practice program

---
+++ dowhileExample9/dowhileExample9/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace dowhileExample9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0;
+            do
+            {
+                x++;
+                if (x == 5)
+                {
+                    x++;
+                    continue;
+                    break;
+                }
+                Console.WriteLine(x + " ");
+
+            } while (x < 10);
+
+        }
+    }
+}
+
