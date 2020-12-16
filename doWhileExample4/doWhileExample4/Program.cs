bf5bd61c025abe8b8778b2b258103bc3f2e7e56f
Akshata Katre <akshataa2sk@gmail.com>
2020-12-16 15:16:29 +0530

    practice program

---
+++ doWhileExample4/doWhileExample4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace doWhileExample4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1234, j = 0;
+            /*ADD CODE HERE */
+            do
+            {
+                j = j + (i % 10);
+            } while ((i = i / 10) != 0);
+
+            Console.WriteLine(j);
+
+        }
+    }
+}
