35e515d08a127486f00491e4375b041667c9f75f
Akshata Katre <akshataa2sk@gmail.com>
2020-12-16 16:09:44 +0530

    practice program

---
+++ WhileLoop7/WhileLoop7/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace WhileLoop7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            float i = 1.0f, j = 0.05f;
+            while (i < 2.0f && j <= 2.0f)
+            {
+                Console.WriteLine(i++ - ++j);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
