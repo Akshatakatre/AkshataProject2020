21ac5ff5dd72ecb4be496016411c6f69b737a66e
Akshata Katre <akshataa2sk@gmail.com>
2020-12-16 15:10:20 +0530

    practice program

---
+++ doWhileExample1/doWhileExample1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace doWhileExample1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1, j = 2, k = 3;
+            do
+            {
+                Console.WriteLine((Convert.ToBoolean(Convert.ToInt32(i++)))
+                && (Convert.ToBoolean(Convert.ToInt32(++j))));
+            } while (i <= 3);
+            Console.ReadKey();
+
+        }
+    }
+}
