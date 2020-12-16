06b93686f1b7d9d60b72aae7c65da170db1d661b
Akshata Katre <akshataa2sk@gmail.com>
2020-12-16 15:21:56 +0530

    practice program

---
+++ doWhileExample8/doWhileExample8/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace doWhileExample8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            for (x = 10; x <= 15; x++)
+                while (Convert.ToBoolean(Convert.ToInt32(x)))
+                {
+                    do
+                    {
+                        Console.WriteLine(1);
+                        if (Convert.ToBoolean(x >> 1))
+                            continue;
+                    } while (Convert.ToBoolean(0));
+                    break;
+                }
+            Console.ReadLine();
+
+        }
+    }
+}
