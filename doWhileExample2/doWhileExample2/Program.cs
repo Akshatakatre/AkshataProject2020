418a71b44b3a98343085c40dd3a60f43f7ac3154
Akshata Katre <akshataa2sk@gmail.com>
2020-12-16 15:14:43 +0530

    practice program

---
+++ doWhileExample2/doWhileExample2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace doWhileExample2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float i = 1.0f, j = 0.05f;
+            do
+            {
+                Console.WriteLine(i++ - ++j);
+            } while (i < 2.0f && j <= 2.0f);
+            Console.ReadKey();
+
+        }
+    }
+}
