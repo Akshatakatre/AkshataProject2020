cc8f25c1fcb8294fdb931341ab0de3ba20dd7306
Akshata Katre <akshataa2sk@gmail.com>
2020-12-16 16:04:34 +0530

    practice program

---
+++ WhileLoop4/WhileLoop4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace WhileLoop4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0;
+            while (x < 20)
+            {
+                while (x < 10)
+                {
+                    if (x % 2 == 0)
+                    {
+                        Console.WriteLine(x);
+                    }
+                    x++;
+                }
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
