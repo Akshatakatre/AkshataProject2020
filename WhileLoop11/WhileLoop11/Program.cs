fec1b0b49383fa2e586e9ba63b0c25863e58d2eb
Akshata Katre <akshataa2sk@gmail.com>
2020-12-16 15:56:31 +0530

    practice program

---
+++ WhileLoop11/WhileLoop11/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace WhileLoop11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            while (i++ != 0) ;
+            Console.WriteLine(i);
+            Console.ReadLine();
+
+        }
+    }
+}
