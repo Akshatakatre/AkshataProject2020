c8e73dd7fedad58753a5200d33771dbce9097f7c
Akshata Katre <akshataa2sk@gmail.com>
2020-12-16 16:13:29 +0530

    practice program

---
+++ WhileLoop9/WhileLoop9/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace WhileLoop9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1, j = 1;
+            while (++i <= 10)
+            {
+                j++;
+            }
+            Console.WriteLine(i + "  " + j);
+            Console.ReadLine();
+
+        }
+    }
+}
