a016fecbffa20a11cba6cc0510ea8e67d93fdc09
Akshata Katre <akshataa2sk@gmail.com>
2020-12-25 13:17:00 +0530

    practice program

---
+++ ArithmeticOperatorEx7/ArithmeticOperatorEx7/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ArithmeticOperatorEx7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 5;
+            int x = 4;
+            int z, c, k;
+            z = 3 * x * x + 2 * x + 4 / x + 8;
+            for (c = 1; c <= n; c++)
+            {
+                for (k = 1; k <= c; k++)
+                {
+                    Console.Write(Convert.ToString(Convert.ToChar(z)));
+                    z++;
+                }
+                Console.WriteLine("\n");
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
