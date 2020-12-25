1a90ca53bf659cf563fdc253132714323c156f39
Akshata Katre <akshataa2sk@gmail.com>
2020-12-25 13:06:38 +0530

    practice program

---
+++ ArithmeticOperatorEx2/ArithmeticOperatorEx2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ArithmeticOperatorEx2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c, x;
+            a = 90;
+            b = 15;
+            c = 3;
+            x = a - b / 3 + c * 2 - 1;
+            Console.WriteLine(x);
+            Console.ReadLine();
+
+        }
+    }
+}
