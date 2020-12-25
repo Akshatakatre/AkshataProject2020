e59ddc056af6f41958faf12e21c919e6d192f5eb
Akshata Katre <akshataa2sk@gmail.com>
2020-12-25 13:04:55 +0530

    practice program

---
+++ ArithmeticOperatorEx1/ArithmeticOperatorEx1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ArithmeticOperatorEx1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float a = 16.4f;
+            int b = 12;
+            float c;
+            c = a * (b + a) / (a - b);
+            Console.WriteLine("result is :" + c);
+            Console.ReadLine();
+
+        }
+    }
+}
