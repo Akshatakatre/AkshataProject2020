bc450db1285cc4da17135e88b142f02c2f9ce248
Akshata Katre <akshataa2sk@gmail.com>
2020-12-25 13:25:23 +0530

    practice program

---
+++ practice4/practice4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace practice4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 9, 4, 1, 3, 8, 6, 7, 2, 1 };
+
+            var nums = numbers.Take(3);
+
+            foreach (var n in nums)
+            {
+
+                Console.WriteLine(n);
+
+            }
+
+
+        }
+    }
+}
