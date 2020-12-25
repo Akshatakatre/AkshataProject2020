5d54e3a597f17b240a2ee93333e7976c77ee1001
Akshata Katre <akshataa2sk@gmail.com>
2020-12-25 13:09:51 +0530

    practice program

---
+++ ArithmeticOperatorEx4/ArithmeticOperatorEx4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ArithmeticOperatorEx4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j = 1, k;
+            for (i = 0; i < 3; i++)
+            {
+                k = j++ - ++j;
+                Console.Write(k + " ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
