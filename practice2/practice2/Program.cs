f0c89c22c0f2ab5a4e221e33fa2f9b82c1d0dd24
Akshata Katre <akshataa2sk@gmail.com>
2020-12-25 13:22:22 +0530

    practice program

---
+++ practice2/practice2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace practice2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] A = { 0, 2, 4, 5, 6, 8 };
+            int[] B = { 1, 3, 5, 7, 8 };
+
+            var nums = A.Union(B);
+
+            foreach (var n in nums)
+            {
+                Console.Write(n +"  ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
