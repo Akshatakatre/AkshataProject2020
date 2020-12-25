f7de914a98e3090f6ec8451d964d78363a9fcf9a
Akshata Katre <akshataa2sk@gmail.com>
2020-12-25 13:20:51 +0530

    practice program

---
+++ practice1/practice1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+
+namespace practice1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] A = { 0, 2, 4, 5, 6, 8, 9 };
+            int[] B = { 1, 3, 5, 7, 8 };
+
+            IEnumerable nums = A.Except(B);
+
+            foreach (var n in nums)
+            {
+                Console.Write(n +"  ");
+            }
+
+        }
+    }
+}
