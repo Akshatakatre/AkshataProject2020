7e9dc9cd1702631645b988a0e24ab66936156834
Akshata Katre <akshataa2sk@gmail.com>
2020-12-25 13:27:26 +0530

    practice program

---
+++ practice5/practice5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace practice5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 9, 10, 0, 11 };
+
+            var nums =
+            from n in numbers
+            select n + 1;
+
+            foreach (var i in nums)
+            {
+                Console.Write(i +",");
+            }
+
+        }
+    }
+}
